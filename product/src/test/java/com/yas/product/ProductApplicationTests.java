051553375b33c5d0e6a04fde732f5f31fc24e93a
Thien Nguyen <thienn@outlook.com>
2022-01-19 12:06:54 +0700

    #4 init product service

---
+++ product/src/test/java/com/yas/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yas.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
