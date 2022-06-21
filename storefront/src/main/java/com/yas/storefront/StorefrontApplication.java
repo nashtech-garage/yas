42c3d10fde329f29f151acc948379425f9d8bfef
Thien Nguyen <thienn@outlook.com>
2022-06-21 16:45:05 +0700

    add storefront

---
+++ storefront/src/main/java/com/yas/storefront/StorefrontApplication.java
@@ -0,0 +1,13 @@
+package com.yas.storefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorefrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorefrontApplication.class, args);
+	}
+
+}
---
+++ storefront/src/test/java/com/yas/storefront/StorefrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yas.storefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorefrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
