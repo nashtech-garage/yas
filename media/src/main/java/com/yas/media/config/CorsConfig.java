5b4752d85c8b13032eef24aeefc8e6d8e846acc9
Thien Nguyen <thienn@outlook.com>
2022-06-23 05:58:40 +0700

    CORS allow all* (need to consider only allow a while list)

---
+++ media/src/main/java/com/yas/media/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.yas.product.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
