0cac9558db4e4aa004d72e47e652384d6f32a666
Thien Nguyen <thienn@outlook.com>
2022-06-28 04:33:18 +0700

    add backoffice.bff

---
+++ backoffice.bff/src/main/java/com/yas/backoffice/bff/Application.java
@@ -0,0 +1,15 @@
+package com.yas.backoffice.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@SpringBootApplication
+@EnableWebFluxSecurity
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
