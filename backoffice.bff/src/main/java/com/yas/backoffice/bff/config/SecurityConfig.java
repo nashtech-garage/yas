1ea481ecb77b61af2063bf84828bc47599924fda
Thien Nguyen <thienn@outlook.com>
2022-06-30 18:05:43 +0700

    disable csrf

--- backoffice.bff/src/main/java/com/yas/backoffice/bff/config/SecurityConfig.java
+++ backoffice.bff/src/main/java/com/yas/backoffice/bff/config/SecurityConfig.java
@@ -2,22 +2,27 @@ package com.yas.backoffice.bff.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-//@Configuration
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .httpBasic().disable()
-//                .formLogin().disable()
-//                .authorizeExchange(exchanges -> exchanges
-//                        .anyExchange().authenticated()
-//                );
-//
-//
-//        return http.build();
-//    }
-//}
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .pathMatchers("/health").permitAll()
+                .anyExchange().authenticated().and()
+                .oauth2Client()
+                .and()
+                .oauth2Login()
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .csrf().disable();
+        return http.build();
+    }
+}
