4db089798b07cf3cd4a0e7912e773345d4047e45
BaoPhamm <phambao1871997@gmail.com>
2022-07-05 15:16:09 +0700

    fix package name mismatch

--- media/src/main/java/com/yas/media/config/CorsConfig.java
+++ media/src/main/java/com/yas/media/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.yas.product.configurations;
+package com.yas.media.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
