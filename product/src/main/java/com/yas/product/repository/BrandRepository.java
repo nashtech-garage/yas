2b2de7c709406e5d9fb8f55dc5d8cbafd7b5d94b
Pham Vu Thien Bao <97822939+BaoPhamm@users.noreply.github.com>
2022-07-29 19:28:26 +0700

    Implement rest api to return list product by brand #51 (#55)

--- product/src/main/java/com/yas/product/repository/BrandRepository.java
+++ product/src/main/java/com/yas/product/repository/BrandRepository.java
@@ -4,7 +4,9 @@ import com.yas.product.model.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BrandRepository extends JpaRepository<Brand,Long> {
-
+    Optional<Brand> findBySlug(String slug);
 }
--- product/src/main/java/com/yas/product/repository/ProductRepository.java
+++ product/src/main/java/com/yas/product/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.yas.product.repository;
 
+import com.yas.product.model.Brand;
 import com.yas.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByBrand(Brand brand);
 }
