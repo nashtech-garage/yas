45efb3565993fa9b1c2046f6c9ddf3872196d66b
Pham Vu Thien Bao <97822939+BaoPhamm@users.noreply.github.com>
2022-08-22 04:49:07 +0700

    Setup media service as a resource server that require access token for authentiction #48 (#59)

---
+++ media/src/main/java/com/yas/media/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.yas.media.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(info = @Info(title = "Media Service API", description = "Media API documentation", version = "1.0"), security = @SecurityRequirement(name = "oauth2_bearer"))
+@SecurityScheme(name = "oauth2_bearer", type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(authorizationCode = @OAuthFlow(authorizationUrl = "${springdoc.oauthflow.authorization-url}", tokenUrl = "${springdoc.oauthflow.token-url}", scopes = {
+                @OAuthScope(name = "openid", description = "openid")
+        })))
+public class SwaggerConfig {
+}
