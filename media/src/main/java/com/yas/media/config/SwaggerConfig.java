fdf8fb5a4fecbd03248b19444b2b58cb244380d1
BaoPhamm <phambao1871997@gmail.com>
2022-09-07 19:18:57 +0700

    Add config SwaggerUI's server url for product and media services

--- media/src/main/java/com/yas/media/config/SwaggerConfig.java
+++ media/src/main/java/com/yas/media/config/SwaggerConfig.java
@@ -8,8 +8,10 @@ import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(info = @Info(title = "Media Service API", description = "Media API documentation", version = "1.0"), security = @SecurityRequirement(name = "oauth2_bearer"))
+@OpenAPIDefinition(info = @Info(title = "Media Service API", description = "Media API documentation", version = "1.0"), security = @SecurityRequirement(name = "oauth2_bearer"),
+        servers = {@Server(url = "${server.servlet.context-path}", description = "Default Server URL")})
 @SecurityScheme(name = "oauth2_bearer", type = SecuritySchemeType.OAUTH2,
         flows = @OAuthFlows(authorizationCode = @OAuthFlow(authorizationUrl = "${springdoc.oauthflow.authorization-url}", tokenUrl = "${springdoc.oauthflow.token-url}", scopes = {
                 @OAuthScope(name = "openid", description = "openid")
