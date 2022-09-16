ed3c1dda9e6f2f80fa291563d5e2cd807648e630
Quoc Minh <quocminhh47@gmail.com>
2022-09-16 10:41:30 +0700

    add unit test for product service

--- product/src/main/java/com/yas/product/model/ProductCategory.java
+++ product/src/main/java/com/yas/product/model/ProductCategory.java
@@ -1,6 +1,8 @@
 package com.yas.product.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,8 @@ import javax.persistence.*;
 @Table(name = "product_category")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
