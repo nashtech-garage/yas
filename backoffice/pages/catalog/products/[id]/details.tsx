b5d09c73f5d926605308f4ad9f74014284efa501
vonhu <vonhu.alien@gmail.com>
2022-09-16 22:07:49 +0700

    design UI for product detail

--- backoffice/pages/catalog/products/[id]/details.tsx
+++ backoffice/pages/catalog/products/[id]/details.tsx
@@ -1,19 +1,92 @@
-import type { NextPage } from 'next'
-import { useRouter } from 'next/router'
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const ProductDetails: NextPage = () => {
-    const router = useRouter()
-    const { id } = router.query
-  
-    return (
-      <>
-      <div className='row mt-5'>
-        <div className='col-md-8'>
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <>
+      <div className="row mt-5">
+        <div className="col-md-8 text-center">
           <h2>Product details {id}</h2>
         </div>
       </div>
-      </>
-    )
-  }
-  
-  export default ProductDetails
+
+      <div className="d-flex flex-row mt-5 gap-5">
+        <div className="d-flex flex-column gap-3">
+          <img
+            src="https://mdbootstrap.com/img/new/standard/city/041.webp"
+            className="img-thumbnail"
+            alt="..."
+            style={{ width: "600px", height: "600px" }}
+          />
+          <span className="text-center fst-italic">hkdfskfkdslf</span>
+        </div>
+
+        <div>
+          <h2 className="mb-2">hfkdfkdsnk</h2>
+          <div className="mb-4 d-flex gap-3">
+            <button type="button" className="btn btn-outline-primary">
+             Brand: djfj
+            </button>
+            <button type="button" className="btn btn-outline-success">Category: djfj</button>
+              
+          </div>
+          <p className="mb-4 text-muted">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
+            vero voluptas voluptates. Expedita magnam veniam ipsa nesciunt hic,
+            eaque commodi impedit tenetur, eveniet alias quia, laudantium
+            explicabo. Rem, officiis ipsam.
+          </p>
+          <p className="mb-4">specification</p>
+          <div className="d-flex flex-column gap-1">
+            <div
+              className="alert alert-primary d-flex justify-content-between"
+              role="alert"
+            >
+              A simple primary alert—check it out!
+              <span className="badge bg-primary text-uppercase">sku</span>
+            </div>
+            <div
+              className="alert alert-secondary d-flex justify-content-between"
+              role="alert"
+            >
+              A simple secondary alert—check it out!
+              <span className="badge bg-secondary text-uppercase">gtin</span>
+            </div>
+            <div
+              className="alert alert-success d-flex justify-content-between"
+              role="alert"
+            >
+              A simple success alert—check it out!
+              <span className="badge bg-success text-uppercase">slug</span>
+            </div>
+            <div
+              className="alert alert-danger d-flex justify-content-between"
+              role="alert"
+            >
+              A simple success alert—check it out!
+              <span className="badge bg-danger text-uppercase">
+                metaKeyword
+              </span>
+            </div>
+            <div
+              className="alert alert-warning d-flex justify-content-between"
+              role="alert"
+            >
+              A simple success alert—check it out!
+              <span className="badge bg-warning text-uppercase">
+                metaDescription
+              </span>
+            </div>
+          </div>
+
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductDetails;
--- backoffice/pages/catalog/products/index.tsx
+++ backoffice/pages/catalog/products/index.tsx
@@ -1,31 +1,30 @@
-import type { NextPage } from 'next'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react';
-import { Button, Table } from 'react-bootstrap';
-import type { Product } from '../../../modules/catalog/models/Product'
-import { getProducts } from '../../../modules/catalog/services/ProductService'
+import type { NextPage } from "next";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Button, Stack, Table } from "react-bootstrap";
+import type { Product } from "../../../modules/catalog/models/Product";
+import { getProducts } from "../../../modules/catalog/services/ProductService";
 
 const ProductList: NextPage = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
-    getProducts()
-      .then((data) => {
-        setProducts(data);
-        setLoading(false);
-      });
+    getProducts().then((data) => {
+      setProducts(data);
+      setLoading(false);
+    });
   }, []);
 
   if (isLoading) return <p>Loading...</p>;
   if (!products) return <p>No product</p>;
-    return (
-      <>
-      <div className='row mt-5'>
-        <div className='col-md-8'>
+  return (
+    <>
+      <div className="row mt-5">
+        <div className="col-md-8">
           <h2>Products</h2>
-        </div> 
-        <div className='col-md-4 text-right'>
+        </div>
+        <div className="col-md-4 text-right">
           <Link href="/catalog/products/create">
             <Button>Create Product</Button>
           </Link>
@@ -40,17 +39,28 @@ const ProductList: NextPage = () => {
           </tr>
         </thead>
         <tbody>
-        {products.map((product) => (
-          <tr key={product.id}>
-            <td>{product.id}</td>
-            <td>{product.name}</td>
-            <td><Link href={`/catalog/products/${product.id}/edit`}><a>Edit</a></Link></td>
-          </tr>
-        ))}
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <td>
+                <Stack direction="horizontal" gap={3}>
+                  <Link href={`/catalog/products/${product.id}/edit`}>
+                    <a>Edit</a>
+                  </Link>
+                  <Button variant="outline-info">
+                    <Link href={`/catalog/products/${product.id}/details`}>
+                      <a>Detail</a>
+                    </Link>
+                  </Button>
+                </Stack>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
-      </>
-    )
-  }
-  
-  export default ProductList
+    </>
+  );
+};
+
+export default ProductList;
