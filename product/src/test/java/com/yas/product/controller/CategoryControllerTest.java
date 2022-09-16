ceb3cd83b46d702990b6f502c2c349eb36a449c1
Phat Le Tuan Diep <phat.letuandiep@nashtechglobal.com>
2022-09-16 18:15:23 +0700

    T41 add Unit test for get category list and get detail category

---
+++ product/src/test/java/com/yas/product/controller/CategoryControllerTest.java
@@ -0,0 +1,102 @@
+package com.yas.product.controller;
+
+import com.yas.product.exception.NotFoundException;
+import com.yas.product.model.Category;
+import com.yas.product.repository.CategoryRepository;
+import com.yas.product.viewmodel.CategoryGetDetailVm;
+import com.yas.product.viewmodel.CategoryGetVm;
+import com.yas.product.viewmodel.CategoryPostVm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class CategoryControllerTest {
+
+    CategoryRepository categoryRepository;
+
+    CategoryController categoryController;
+
+    List<Category> categories;
+    Category category;
+
+    Principal principal;
+    CategoryPostVm categoryPostVm;
+    UriComponentsBuilder uriComponentsBuilder;
+
+    @BeforeEach
+    void setUp() {
+        uriComponentsBuilder = mock(UriComponentsBuilder.class);
+        principal = mock(Principal.class);
+        categories = new ArrayList<>();
+        categoryRepository = mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        category = new Category();
+        category.setId(1L);
+        category.setName("hô hô");
+        category.setSlug("ho-ho");
+        category.setMetaKeyword("ho ho");
+        category.setMetaDescription("ho ho");
+        category.setDisplayOrder((short) 0);
+        categories.add(category);
+        when(principal.getName()).thenReturn("user");
+        categoryPostVm = new CategoryPostVm(
+                "name",
+                "slug",
+                "description",
+                null,
+                "",
+                "",
+                (short) 0
+        );
+
+    }
+
+    @Test
+    void ListCategories_ValidListCategoryGetVM_Success() {
+        List<CategoryGetVm> expect = List.of(
+                new CategoryGetVm(1L, "hô hô", "ho-ho")
+        );
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+        ResponseEntity<List<CategoryGetVm>> actual = categoryController.listCategories();
+        assertThat(Objects.requireNonNull(actual.getBody()).size()).isEqualTo(expect.size());
+        assertThat(actual.getBody().get(0).id()).isEqualTo(expect.get(0).id());
+    }
+
+    @Test
+    void getCategory_NotFoundCategoryGetDetailVM_ThrowNotFoundException() {
+        when(categoryRepository.findById(2L)).thenThrow(NotFoundException.class);
+        assertThrows(NotFoundException.class, () -> categoryController.getCategory(2L));
+    }
+
+    @Test
+    void getCategory_ValidCategoryGetDetailVM_Success() {
+        when(categoryRepository.findById(1L)).thenReturn(Optional.of(category));
+        CategoryGetDetailVm expect = CategoryGetDetailVm.fromModel(category);
+        ResponseEntity<CategoryGetDetailVm> actual = categoryController.getCategory(1L);
+        assertEquals(Objects.requireNonNull(actual.getBody()).Id(), expect.Id());
+    }
+
+    @Test
+    void createCategory_ValidCategoryWithOutParentId_Success() {
+    }
+
+    @Test
+    void updateCategory() {
+    }
+}
