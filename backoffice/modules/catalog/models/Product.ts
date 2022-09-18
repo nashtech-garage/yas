2c12d840291dba749a27fdc16842a3439f183579
vonhu <vonhu.alien@gmail.com>
2022-09-18 18:56:03 +0700

    fix backoffice

--- backoffice/modules/catalog/models/Product.ts
+++ backoffice/modules/catalog/models/Product.ts
@@ -1,13 +1,13 @@
 export type Product = {
   id: number;
   name: string;
-  shortDescription: string;
-  description: string;
-  specification: string;
-  sku: string;
-  gtin: string;
-  slug: string;
-  metaKeyword: string;
-  metaDescription: string;
-  thumbnailMediaUrl: string;
+  shortDescription?: string;
+  description?: string;
+  specification?: string;
+  sku?: string;
+  gtin?: string;
+  slug?: string;
+  metaKeyword?: string;
+  metaDescription?: string;
+  thumbnailMediaUrl?: string;
 };
