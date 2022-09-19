a005b57622f1e06b1ce2a64c101e2430dc7f8fb7
Thien Nguyen <thienn@outlook.com>
2022-09-19 08:15:50 +0700

    [storefront] add env configs

--- storefront/modules/catalog/services/CategoryService.ts
+++ storefront/modules/catalog/services/CategoryService.ts
@@ -1,11 +1,11 @@
 import { Category } from '../models/Category'
 
 export async function getCategories(): Promise<Category[]> {
-  const response = await fetch('http://storefront/api/product/storefront/categories');
+  const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_PATH + "/product/storefront/categories");
   return await response.json();
 }
 
 export async function getCategory(id: number){
-  const response = await fetch('http://storefront/api/product/storefront/categories/' + id);
+  const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_PATH + "/product/storefront/categories/" + id);
   return await response.json();
 }
--- storefront/modules/catalog/services/ProductService.ts
+++ storefront/modules/catalog/services/ProductService.ts
@@ -2,13 +2,11 @@ import { Product } from "../models/Product";
 import { ProductThumbnail } from "../models/ProductThumbnail";
 
 export async function getFeaturedProducts(): Promise<ProductThumbnail[]> {
-  const response = await fetch(
-    "http://storefront/api/product/storefront/products/featured"
-  );
+  const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_PATH + "/product/storefront/products/featured");
   return await response.json();
 }
 
 export async function getProduct(slug: string): Promise<Product> {
-  const response = await fetch("http://storefront/api/product/storefront/products/" + slug);
+  const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_PATH + "/product/storefront/products/" + slug);
   return response.json();
 }
