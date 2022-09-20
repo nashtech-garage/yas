2d7a9932083d8887c2e500a55af6d6b7c8ceb7ee
Hao Nguyen Tan <hao.nguyentan@nashtechglobal.com>
2022-09-20 09:52:11 +0700

    remove unused import

--- product/src/main/java/com/yas/product/viewmodel/ProductPostVm.java
+++ product/src/main/java/com/yas/product/viewmodel/ProductPostVm.java
@@ -1,9 +1,6 @@
 package com.yas.product.viewmodel;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public record ProductPostVm(
