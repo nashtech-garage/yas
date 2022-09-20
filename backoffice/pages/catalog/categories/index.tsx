858e9fd571e476ff761875c8619090e2bc5dae73
Phat Le Tuan Diep <phat.letuandiep@nashtechglobal.com>
2022-09-20 13:34:54 +0700

    add hierarchy for list category in backoffice, change add attribute for vm when sen data to frontend, fix unit test

--- backoffice/pages/catalog/categories/index.tsx
+++ backoffice/pages/catalog/categories/index.tsx
@@ -1,55 +1,75 @@
-import type { NextPage } from 'next'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react';
-import { Button, Table } from 'react-bootstrap';
-import type { Category } from '../../../modules/catalog/models/Category'
-import { getCategories } from '../../../modules/catalog/services/CategoryService'
+import type { NextPage } from "next";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { Button, Table } from "react-bootstrap";
+import type { Category } from "../../../modules/catalog/models/Category";
+import { getCategories } from "../../../modules/catalog/services/CategoryService";
 
 const CategoryList: NextPage = () => {
   const [categories, setCategories] = useState<Category[]>([]);
   const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
-    getCategories()
-      .then((data) => {
-        setCategories(data);
-        setLoading(false);
-      });
+    getCategories().then((data) => {
+      setCategories(data);
+      setLoading(false);
+    });
   }, []);
 
   if (isLoading) return <p>Loading...</p>;
   if (!categories) return <p>No category</p>;
 
+  const renderCategoriesHierarchy: Function = (
+    id: Number,
+    list: Array<Category>,
+    parentHierarchy: String
+  ) => {
+    const renderArr = list.filter((e) => e.parentId == id);
+    const newArr = list.filter((e) => e.parentId != id);
+    return renderArr
+      .sort((a: Category, b: Category) => a.name.localeCompare(b.name))
+      .map((category: Category) => {
+        return (
+          <React.Fragment key={category.id}>
+            <tr>
+              <td>{category.id}</td>
+              <td>{parentHierarchy}{category.name}</td>
+            </tr>
+            {renderCategoriesHierarchy(
+              category.id,
+              newArr,
+              parentHierarchy + category.name + " >> "
+            )}
+          </React.Fragment>
+        );
+      });
+  };
+
   return (
     <>
-    <div className='row mt-5'>
-      <div className='col-md-8'>
-        <h2>Categories</h2>
-      </div> 
-      <div className='col-md-4 text-right'>
-        <Link href="/catalog/categories/create">
-          <Button>Create Category</Button>
-        </Link>
+      <div className="row mt-5">
+        <div className="col-md-8">
+          <h2>Categories</h2>
+        </div>
+        <div className="col-md-4 text-right">
+          <Link href="/catalog/categories/create">
+            <Button>Create Category</Button>
+          </Link>
+        </div>
       </div>
-    </div>
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {categories.map((category) => (
-          <tr key={category.id}>
-            <td>{category.id}</td>
-            <td>{category.name}</td>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {renderCategoriesHierarchy(-1, categories, "")}
         </tbody>
-    </Table>
+      </Table>
     </>
-  )
-}
+  );
+};
 
-export default CategoryList
+export default CategoryList;
