0f818415caaa2530d07d43bb50098347f3a3f50c
Ninh Bui Van <ninhbuise@gmail.com>
2022-09-30 10:07:15 +0700

    resolve sonarcloud security

--- customer/src/main/java/com/yas/customer/service/CustomerService.java
+++ customer/src/main/java/com/yas/customer/service/CustomerService.java
@@ -9,7 +9,6 @@ import com.yas.customer.exception.WrongEmailFormatException;
 import com.yas.customer.viewmodel.CustomerAdminVm;
 import com.yas.customer.viewmodel.CustomerVm;
 import com.yas.customer.viewmodel.GuestUserVm;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.UsersResource;
@@ -18,6 +17,8 @@ import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.ForbiddenException;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,7 +29,6 @@ public class CustomerService {
     private  final KeycloakClientConfig keycloakClientConfig;
     private final KeycloakPropsConfig keycloakPropsConfig;
     private static final String ERROR_FORMAT = "%s: Client %s don't have access right for this resource";
-    private static final String PASSWORD_HARDCODED = "GUEST";
 
     public CustomerService(Keycloak keycloak, KeycloakPropsConfig keycloakPropsConfig, KeycloakClientConfig keycloakClientConfig) {
         this.keycloak = keycloak;
@@ -74,9 +74,9 @@ public class CustomerService {
     public GuestUserVm createGuestUser() {
         try {
             UsersResource resource = keycloakClientConfig.getAdminKeyCloak().realm(keycloakPropsConfig.getRealm()).users();
-            String randomGuestName = RandomStringUtils.randomAlphanumeric(12);
+            String randomGuestName = generateSafeString();
             String guestUserEmail = randomGuestName + "_guest@yas.com";
-            CredentialRepresentation credential = createPasswordCredentials(PASSWORD_HARDCODED);
+            CredentialRepresentation credential = createPasswordCredentials("GUEST");
 
             UserRepresentation user = new UserRepresentation();
             user.setUsername(guestUserEmail);
@@ -88,7 +88,7 @@ public class CustomerService {
             user.setEnabled(true);
 
             resource.create(user);
-            return new GuestUserVm(guestUserEmail, PASSWORD_HARDCODED);
+            return new GuestUserVm(guestUserEmail, "GUEST");
         } catch (InternalError exception) {
             throw new InternalErrorException(exception.getMessage());
         }
@@ -100,5 +100,13 @@ public class CustomerService {
         passwordCredentials.setType(CredentialRepresentation.PASSWORD);
         passwordCredentials.setValue(password);
         return passwordCredentials;
+    }
+
+    private String generateSafeString() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[12];
+        random.nextBytes(bytes);
+        Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+        return encoder.encodeToString(bytes);
     }
 }
