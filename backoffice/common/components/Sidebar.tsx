15c4599ce439e16765eb6d8e8d938dd5a6cc533f
Tan Hao Nguyen <101847895+tanhao111@users.noreply.github.com>
2022-10-06 13:05:39 +0700

    [backoffice] view customer list (#163)
    * Done view customer list

--- backoffice/common/components/Sidebar.tsx
+++ backoffice/common/components/Sidebar.tsx
@@ -3,19 +3,41 @@ import Link from 'next/link'
 
 export default function Sidebar(){
   return (
-    <nav id="sidebarMenu" className='col-md-3 col-lg-2 d-md-block bg-light sidebar collapse'>
+    <nav
+      id="sidebarMenu"
+      className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+    >
       <div className="position-sticky pt-3">
+        <ul className="nav flex-column mb-2">
+          <li className="nav-item">
+            <Link href="/customers">
+              <a className="nav-link">
+                <span
+                  data-feather="file-text"
+                  className="align-text-bottom"
+                ></span>
+                Customers
+              </a>
+            </Link>
+          </li>
+        </ul>
         <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
           <span>Catalog</span>
           <a className="link-secondary" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle" className="align-text-bottom"></span>
+            <span
+              data-feather="plus-circle"
+              className="align-text-bottom"
+            ></span>
           </a>
         </h6>
         <ul className="nav flex-column mb-2">
           <li className="nav-item">
-            <Link href='/catalog/categories'>
+            <Link href="/catalog/categories">
               <a className="nav-link">
-                <span data-feather="file-text" className="align-text-bottom"></span>
+                <span
+                  data-feather="file-text"
+                  className="align-text-bottom"
+                ></span>
                 Categories
               </a>
             </Link>
@@ -23,15 +45,21 @@ export default function Sidebar(){
           <li className="nav-item">
             <Link href="/catalog/products">
               <a className="nav-link">
-                <span data-feather="file-text" className="align-text-bottom"></span>
+                <span
+                  data-feather="file-text"
+                  className="align-text-bottom"
+                ></span>
                 Products
-              </a>         
+              </a>
             </Link>
           </li>
           <li className="nav-item">
             <Link href="/catalog/brands">
               <a className="nav-link">
-                <span data-feather="file-text" className="align-text-bottom"></span>
+                <span
+                  data-feather="file-text"
+                  className="align-text-bottom"
+                ></span>
                 Brands
               </a>
             </Link>
@@ -39,7 +67,10 @@ export default function Sidebar(){
           <li className="nav-item">
             <Link href="/catalog/product-attributes">
               <a className="nav-link">
-                <span data-feather="file-text" className="align-text-bottom"></span>
+                <span
+                  data-feather="file-text"
+                  className="align-text-bottom"
+                ></span>
                 Product Attributes
               </a>
             </Link>
---
+++ backoffice/modules/customer/models/Customers.ts
@@ -0,0 +1,7 @@
+import { Customer } from './Customer';
+
+export type Customers = {
+  customers: Customer[]
+  totalPage: number;
+  totalUser: number;
+};
