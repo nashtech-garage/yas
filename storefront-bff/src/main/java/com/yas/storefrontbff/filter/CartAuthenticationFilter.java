346431d198712f9d4c910823be8b6e24d24cc885
Pham Vu Thien Bao <phambao1871997@gmail.com>
2022-10-07 18:02:41 +0700

    Store guest user information into web session cookies

--- storefront-bff/src/main/java/com/yas/storefrontbff/filter/CartAuthenticationFilter.java
+++ storefront-bff/src/main/java/com/yas/storefrontbff/filter/CartAuthenticationFilter.java
@@ -9,7 +9,6 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
@@ -24,6 +23,8 @@ import reactor.core.publisher.Mono;
 import reactor.util.annotation.NonNull;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -34,6 +35,8 @@ public class CartAuthenticationFilter implements WebFilter {
     private final ServiceUrlConfig serviceUrlConfig;
     private final ReactiveClientRegistrationRepository clientRegistrationRepository;
     private static final String GRANT_TYPE = "grant_type";
+    private static final String URL_API_POST_CART = "/api/cart/storefront/carts";
+    private static final String GUEST_INFO_KEY = "GUEST_INFOMATION";
 
     public CartAuthenticationFilter(WebClient webClient,
                                     ServiceUrlConfig serviceUrlConfig,
@@ -48,9 +51,18 @@ public class CartAuthenticationFilter implements WebFilter {
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
 
         ServerHttpRequest request = exchange.getRequest();
-        if (request.getPath().toString().contains("/api/cart/storefront/carts")
+        if (request.getPath().toString().contains(URL_API_POST_CART)
                 && Objects.equals(request.getMethod(), HttpMethod.POST)) {
             AtomicBoolean isAuthenticated = new AtomicBoolean(false);
+            AtomicBoolean isAuthenticatedAsGuest = new AtomicBoolean(false);
+            HashMap<String, String> guestInfoFromCookie = new LinkedHashMap<>();
+
+            exchange.getSession().subscribe(session -> {
+                if (session.getAttributes().containsKey(GUEST_INFO_KEY)) {
+                    isAuthenticatedAsGuest.set(true);
+                    guestInfoFromCookie.putAll((LinkedHashMap<String, String>) session.getAttributes().get(GUEST_INFO_KEY));
+                }
+            });
 
             return ReactiveSecurityContextHolder.getContext()
                     .map(SecurityContext::getAuthentication)
@@ -58,12 +70,21 @@ public class CartAuthenticationFilter implements WebFilter {
                     .doOnNext(principal -> isAuthenticated.set(true))
                     .doOnError(Throwable::printStackTrace)
                     .then(Mono.defer(() -> {
-                        if (!isAuthenticated.get()) {
+                        if (isAuthenticated.get()) {
+                            return chain.filter(exchange);
+                        } else if (isAuthenticatedAsGuest.get()) {
+                            exchange.getRequest()
+                                    .mutate()
+                                    .headers(h -> h.setBearerAuth(guestInfoFromCookie.get("accessToken")))
+                                    .build();
+                            return chain.filter(exchange);
+                        } else {
                             MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-                            ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak").block();
                             formData.add(GRANT_TYPE, "client_credentials");
-                            formData.add("client_id", clientRegistration.getClientId());
-                            formData.add("client_secret", clientRegistration.getClientSecret());
+                            clientRegistrationRepository.findByRegistrationId("keycloak").subscribe(clientRegistration -> {
+                                formData.add("client_id", clientRegistration.getClientId());
+                                formData.add("client_secret", clientRegistration.getClientSecret());
+                            });
 
                             return getToken(formData).map(clientToken -> createGuestUser(clientToken.access_token())
                                     .map(createdUserVm -> {
@@ -71,14 +92,23 @@ public class CartAuthenticationFilter implements WebFilter {
                                         formData.add(GRANT_TYPE, "password");
                                         formData.add("username", createdUserVm.email());
                                         formData.add("password", createdUserVm.password());
-                                        return getToken(formData).doOnNext(userToken -> exchange
-                                                .getRequest()
-                                                .mutate()
-                                                .headers(h -> h.setBearerAuth(userToken.access_token()))
-                                                .build()).thenReturn(exchange);
-                                    })).flatMap(monox3Void -> monox3Void.flatMap(monox2Void -> monox2Void.flatMap(chain::filter)));
+
+                                        return getToken(formData).map(userToken -> {
+                                            HashMap<String, String> guestInfo = new LinkedHashMap<>();
+                                            guestInfo.put("accessToken", userToken.access_token());
+                                            guestInfo.put("userId", createdUserVm.userId());
+                                            guestInfo.put("email", createdUserVm.email());
+                                            guestInfo.put("password", createdUserVm.password());
+
+                                            return exchange.getSession().map(session -> {
+                                                session.getAttributes().put(GUEST_INFO_KEY, guestInfo);
+                                                request.mutate().headers(h -> h.setBearerAuth(userToken.access_token())).build();
+
+                                                return exchange.mutate().request(request).build();
+                                            });
+                                        });
+                                    })).flatMap(monox4Void -> monox4Void.flatMap(monox3Void -> monox3Void.flatMap(monox2Void -> monox2Void.flatMap(chain::filter))));
                         }
-                        return chain.filter(exchange);
                     }));
         }
         return chain.filter(exchange);
