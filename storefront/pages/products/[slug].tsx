a279c59395c823f024a6a0d85b1f262cc3e7d29d
Thien Nguyen <thienn@outlook.com>
2022-10-09 13:48:18 +0700

    hide add to favourite

--- storefront/pages/products/[slug].tsx
+++ storefront/pages/products/[slug].tsx
@@ -87,7 +87,7 @@ const ProductDetails = ({ product }: Props) => {
               <span className="badge bg-warning text-uppercase">metaDescription</span>
             </div>
             <div className="d-flex justify-content-between">
-              <button type="button" className="btn btn-danger">
+{/*               <button type="button" className="btn btn-danger">
                 {' '}
                 <span>
                   <svg
@@ -102,7 +102,7 @@ const ProductDetails = ({ product }: Props) => {
                   </svg>
                 </span>
                 Add to favourite
-              </button>
+              </button> */}
               <form onSubmit={handleAddToCart}>
                 <input type={'hidden'} name={'productId'} value={product.id} />
                 <input type={'hidden'} name={'quantity'} value={1} />
