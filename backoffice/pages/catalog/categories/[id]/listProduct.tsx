87840c035cab9ec06a7759de7a648313d90c8613
khanh.dinhtrong <19127437@student.hcmus.edu.vn>
2022-10-10 18:14:17 +0700

    check fail

--- backoffice/pages/catalog/categories/[id]/listProduct.tsx
+++ backoffice/pages/catalog/categories/[id]/listProduct.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { Stack ,Table } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import React, { useEffect, useState } from 'react';
 import { ProductThumbnail } from '../../../../modules/catalog/models/ProductThumbnail';
 import { getProductsByCategory} from '../../../../modules/catalog/services/CategoryService';
