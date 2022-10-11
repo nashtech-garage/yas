3b343145313b11b1f912b8d06d4978e08bb0c0f6
Pham Vu Thien Bao <phambao1871997@gmail.com>
2022-10-11 19:49:24 +0700

    fix sonar check

--- storefront-bff/src/main/java/com/yas/storefrontbff/controller/CartController.java
+++ storefront-bff/src/main/java/com/yas/storefrontbff/controller/CartController.java
@@ -1,14 +1,12 @@
 package com.yas.storefrontbff.controller;
 
 import com.yas.storefrontbff.service.CartService;
-import com.yas.storefrontbff.viewmodel.CartGetDetailVm;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
@@ -37,9 +35,7 @@ public class CartController {
                     .map(OAuth2AuthorizedClient::getAccessToken)
                     .map(OAuth2AccessToken::getTokenValue)
                     .flatMap(userToken -> cartService.moveGuestCartToUserCart(userToken, guestInfoFromCookie.get("accessToken")))
-                    .doOnSuccess(s -> {
-                        session.getAttributes().remove(GUEST_INFO_KEY);
-                    });
+                    .doOnSuccess(s -> session.getAttributes().remove(GUEST_INFO_KEY));
         }
         return Mono.empty();
     }
