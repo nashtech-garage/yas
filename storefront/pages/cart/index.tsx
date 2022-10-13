b5fae46b5f5d575567c34057b353c92089742eb9
Quoc Minh <quocminhh47@gmail.com>
2022-10-13 23:18:56 +0700

    remove unwanted code

--- storefront/pages/cart/index.tsx
+++ storefront/pages/cart/index.tsx
@@ -3,47 +3,6 @@ import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 
 const Cart: NextPage = () => {
-  const products = [
-    {
-      id: 2,
-      name: 'iphone 14',
-      price: 1500,
-      url: 'https://www.duchuymobile.com/images/variant_image/47/iphone-13-pro-max-den.jpeg',
-      quantity: 1,
-    },
-    {
-      id: 3,
-      name: 'iphone 13',
-      price: 1000,
-      url: 'https://www.duchuymobile.com/images/variant_image/47/iphone-13-pro-max-den.jpeg',
-      quantity: 2,
-    },
-    {
-      id: 1,
-      name: 'iphone 12',
-      price: 500,
-      url: 'https://www.duchuymobile.com/images/variant_image/47/iphone-13-pro-max-den.jpeg',
-      quantity: 3,
-    },
-  ];
-
-  const handleMinus = (e: any) => {
-    console.log(e.target.id.slice(-1));
-    let productId = e.target.id.slice(-1);
-    const input = document.getElementById(`quantity-${productId}`) as HTMLInputElement | null;
-    const oldValue: number = Number(input?.value);
-    if (input != null) input.value = String(oldValue - 1);
-    if (input != null && oldValue != null && oldValue == 0) input.value = String(0);
-  };
-
-  const handlePlus = (e: any) => {
-    console.log(e.target.id.slice(-1));
-    let productId = e.target.id.slice(-1);
-    const input = document.getElementById(`quantity-${productId}`) as HTMLInputElement | null;
-    const oldValue: number = Number(input?.value);
-    if (input != null) input.value = String(oldValue + 1);
-  };
-
   return (
     <section className="shop-cart spad">
       <div className="container">
@@ -61,59 +20,57 @@ const Cart: NextPage = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {products.map((product) => (
-                    <tr key={product.id}>
-                      <td className="cart__product__item">
-                        <img
-                          src={product.url}
-                          alt="img"
-                          style={{ width: '85px', height: '85px' }}
-                        />
-                        <div className="cart__product__item__title">
-                          <h6>{product.name}</h6>
+                  <tr>
+                    <td className="cart__product__item">
+                      <img
+                        src="https://www.duchuymobile.com/images/variant_image/47/iphone-13-pro-max-den.jpeg"
+                        alt="img"
+                        style={{ width: '85px', height: '85px' }}
+                      />
+                      <div className="cart__product__item__title">
+                        <h6>Iphone 14</h6>
+                      </div>
+                    </td>
+                    <td className="cart__price">34.500.000</td>
+                    <td className="cart__quantity">
+                      <div className="pro-qty">
+                        <div className="quantity buttons_added">
+                          <input
+                            id="minus-button"
+                            type="button"
+                            value="-"
+                            className="minus"
+                            // onClick={(e) => handleMinus(e)}
+                          />
+                          <input
+                            id="quanity"
+                            type="number"
+                            step="1"
+                            min="1"
+                            max=""
+                            name="quantity"
+                            defaultValue={1}
+                            title="Qty"
+                            className="input-text qty text"
+                          />
+                          <input
+                            id="plus-button"
+                            type="button"
+                            value="+"
+                            className="plus"
+                            // onClick={(e) => handlePlus(e)}
+                          />
                         </div>
-                      </td>
-                      <td className="cart__price">{product.price}</td>
-                      <td className="cart__quantity">
-                        <div className="pro-qty">
-                          <div className="quantity buttons_added">
-                            <input
-                              id={`minus-${product.id}`}
-                              type="button"
-                              value="-"
-                              className="minus"
-                              onClick={(e) => handleMinus(e)}
-                            />
-                            <input
-                              id={`quantity-${product.id}`}
-                              type="number"
-                              step="1"
-                              min="1"
-                              max=""
-                              name="quantity"
-                              defaultValue={product.quantity}
-                              title="Qty"
-                              className="input-text qty text"
-                            />
-                            <input
-                              id={`plus-${product.id}`}
-                              type="button"
-                              value="+"
-                              className="plus"
-                              onClick={(e) => handlePlus(e)}
-                            />
-                          </div>
-                        </div>
-                      </td>
-                      <td className="cart__total">{product.price * product.quantity} </td>
-                      <td className="cart__close">
-                        {' '}
-                        <button className="remove_product">
-                          <i className="bi bi-x-lg"></i>
-                        </button>{' '}
-                      </td>
-                    </tr>
-                  ))}
+                      </div>
+                    </td>
+                    <td className="cart__total">34500000 </td>
+                    <td className="cart__close">
+                      {' '}
+                      <button className="remove_product">
+                        <i className="bi bi-x-lg"></i>
+                      </button>{' '}
+                    </td>
+                  </tr>
                 </tbody>
               </table>
             </div>
