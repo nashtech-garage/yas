ea9e16c849a5cfadec2188780943d51c6b14c7d9
Hau Chau Van <101496150+vanhau293@users.noreply.github.com>
2022-10-14 06:24:52 +0700

    #195 CRUD Product Attribute Group UI (#198)
    * #195 CRUD Product Attribute Group UI

--- backoffice/modules/catalog/services/ProductAttributeGroupService.ts
+++ backoffice/modules/catalog/services/ProductAttributeGroupService.ts
@@ -1,6 +1,31 @@
+import { stringify } from 'querystring';
 import { ProductAttributeGroup } from '../models/ProductAttributeGroup';
 
 export async function getProductAttributeGroups(): Promise<ProductAttributeGroup[]> {
   const response = await fetch('/api/product/backoffice/product-attribute-groups');
   return await response.json();
+}
+export async function getProductAttributeGroup(id: number): Promise<ProductAttributeGroup> {
+  const response = await fetch('/api/product/backoffice/product-attribute-groups/' + id);
+  return await response.json();
+}
+export async function createProductAttributeGroup(productAttributeGroup: ProductAttributeGroup) {
+  const response = await fetch('/api/product/backoffice/product-attribute-groups', {
+    method: 'POST',
+    body: JSON.stringify(productAttributeGroup),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  });
+  return await response.json();
+}
+export async function updateProductAttributeGroup(
+  id: number,
+  productAttributeGroup: ProductAttributeGroup
+) {
+  const response = await fetch('/api/product/backoffice/product-attribute-groups/' + id, {
+    method: 'PUT',
+    body: JSON.stringify(productAttributeGroup),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  });
+  if (response.status === 204) return await response;
+  else return await response.json();
 }
---
+++ backoffice/pages/catalog/product-attribute-groups/create.tsx
@@ -0,0 +1,56 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import { createProductAttributeGroup } from '../../../modules/catalog/services/ProductAttributeGroupService';
+import { ProductAttributeGroup } from '../../../modules/catalog/models/ProductAttributeGroup';
+const ProductAttributeGroupCreate: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+  const handleSubmitGroup = async (event: any) => {
+    let productAttributeGroup: ProductAttributeGroup = {
+      id: 0,
+      name: event.name,
+    };
+    productAttributeGroup = await createProductAttributeGroup(productAttributeGroup);
+    router.replace('/catalog/product-attribute-groups');
+  };
+
+  return (
+    <>
+      <div className="row mt-5">
+        <div className="col-md-8">
+          <h2>Create Product Attribute Group</h2>
+          <form onSubmit={handleSubmit(handleSubmitGroup)}>
+            <div className="mb-3">
+              <div className="mb-3">
+                <label className="form-label">Name</label>
+                <input
+                  className="form-control"
+                  {...register('name', { required: true })}
+                  type="text"
+                  id="name"
+                  name="name"
+                />
+                {errors.name && errors.name.type == 'required' && (
+                  <p className="text-danger">Please enter the name product attribute group</p>
+                )}
+              </div>
+            </div>
+            <button className="btn btn-primary" type="submit">
+              Save
+            </button>
+            &emsp;
+            <Link href="/catalog/product-attribute-groups">
+              <button className="btn btn-outline-secondary">Cancel</button>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductAttributeGroupCreate;
