279564d0f921b52b37ba74a857ccd6b599a9ed5f
quocminhh47 <103627485+quocminhh47@users.noreply.github.com>
2022-10-24 04:06:49 +0700

    enhance homepage UI and add price attribute to product info (#213)

--- storefront/pages/_app.tsx
+++ storefront/pages/_app.tsx
@@ -16,6 +16,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
         ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
       </Head>
       <Layout>
         <Component {...pageProps} />
