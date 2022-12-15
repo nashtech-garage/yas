db53b536296d1d7898d7147a7cc08c605a29f545
Thien Nguyen Le Quynh <nlqthien@gmail.com>
2022-12-15 17:15:09 +0700

    [backoffice] new sidebar (#240)
    * [backoffice] new sidebar

--- backoffice/pages/_app.tsx
+++ backoffice/pages/_app.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Layout from '../common/components/Layout';
+import Script from 'next/script';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
---
+++ backoffice/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
