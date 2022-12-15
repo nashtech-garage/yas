3ce1895cfa097e49656f892d39a761a8d6c9e645
Thien Nguyen <thienn@outlook.com>
2022-12-15 16:07:34 +0700

    [backoffice] new sidebar

--- backoffice/common/components/Layout.tsx
+++ backoffice/common/components/Layout.tsx
@@ -4,7 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import styles from '../../styles/Layout.module.css';
 import AuthenticationInfo from './AuthenticationInfo';
 import { useRef, useState } from 'react';
-import Sidebar from './Sidebar';
+import Link from 'next/link';
 
 type Props = {
   children: React.ReactNode;
@@ -21,23 +21,80 @@ export default function Layout({ children }: Props) {
         <meta name="description" content="Yet another shop backoffice" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar collapseOnSelect bg="dark" variant="dark" className={styles.header}>
-        {/* <div className="container-fluid"> */}
-        <Navbar.Brand href="/" className="font-weight-black text-white">
-          Yas - Backoffice
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <AuthenticationInfo />
-          </Navbar.Text>
-        </Navbar.Collapse>
-        {/* </div> */}
-      </Navbar>
-      <div className="container-fluid">
-        <Sidebar />
-        <div id={styles.rightBody}>
-          <main className="col-md-9 py-5 container-fluid" id={styles.main}>
+      <div className="wrapper d-flex align-items-stretch">
+        <nav id="sidebar">
+          <div className="p-4 pt-5">
+            <h1>
+              <a href="/" className="logo">
+                Yas
+              </a>
+            </h1>
+            <ul className="list-unstyled components mb-5">
+              <li className="active">
+                <a
+                  href="#catalogSubmenu"
+                  data-target="#catalogSubmenu"
+                  data-bs-toggle="collapse"
+                  aria-controls="catalogSubmenu"
+                  aria-expanded="false"
+                  className="dropdown-toggle"
+                >
+                  <span className="fa fa-product-hunt" aria-hidden="true"></span> Catalog
+                </a>
+                <ul className="collapse show list-unstyled" id="catalogSubmenu">
+                  <li>
+                    <Link href="/catalog/brands">Brands</Link>
+                  </li>
+                  <li>
+                    <Link href="/catalog/categories">Catgegories</Link>
+                  </li>
+                  <li>
+                    <Link href="/catalog/products">Products</Link>
+                  </li>
+                  <li>
+                    <Link href="/catalog/product-options">Product Options</Link>
+                  </li>
+                  <li>
+                    <Link href="/catalog/product-attributes">Product Attributes</Link>
+                  </li>
+                  <li>
+                    <Link href="/catalog/product-attribute-groups">Product Attributes Group</Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a
+                  href="#customerSubmenu"
+                  data-target="#customerSubmenu"
+                  data-bs-toggle="collapse"
+                  aria-controls="customerSubmenu"
+                  aria-expanded="false"
+                  className="dropdown-toggle"
+                >
+                  <span className="fa fa-user"></span> Cusomters
+                </a>
+                <ul className="collapse list-unstyled" id="customerSubmenu">
+                  <li>
+                    <Link href="/customers">Customers</Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div id="content">
+          <Navbar collapseOnSelect bg="dark" variant="dark" className={styles.header}>
+            <button type="button" id="sidebarCollapse" className="btn btn-primary">
+              <i className="fa fa-bars"></i>
+              <span className="sr-only">Toggle Menu</span>
+            </button>
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                <AuthenticationInfo />
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Navbar>
+          <main className="container-fluid" id={styles.main}>
             {children}
           </main>
           <footer className={styles.footer}>
--- backoffice/pages/_app.tsx
+++ backoffice/pages/_app.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Layout from '../common/components/Layout';
+import Script from 'next/script';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
@@ -14,7 +15,22 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
       </Head>
+      <Script
+        src="https://code.jquery.com/jquery-3.6.2.min.js"
+        integrity="sha256-2krYZKh//PcchRtd+H+VyyQoZ/e3EcrkxhM8ycwASPA="
+        crossorigin="anonymous"
+        onLoad={() => {
+          jQuery('#sidebarCollapse').on('click', function () {
+            jQuery('#sidebar').toggleClass('active');
+          });
+        }}
+      />
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
