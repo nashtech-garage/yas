2eed1b4d8419f1873102d73160eb07189dacdaa9
Tan Hao Nguyen <101847895+tanhaok@users.noreply.github.com>
2023-02-07 06:18:37 +0700

    [storefront-bff] upgrade spring boot to v3 (#250)

--- storefront-bff/src/main/java/com/yas/storefrontbff/config/ServiceUrlConfig.java
+++ storefront-bff/src/main/java/com/yas/storefrontbff/config/ServiceUrlConfig.java
@@ -1,11 +1,9 @@
 package com.yas.storefrontbff.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
 import java.util.Map;
 
-@ConstructorBinding
 @ConfigurationProperties(prefix = "yas")
 public record ServiceUrlConfig(
         Map<String, String> services) {
