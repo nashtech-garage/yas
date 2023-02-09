e14054fde032c37a20d995021ece82ba3478f71b
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-02-09 16:37:38 +0700

    Optimize and add some validation for product general information tab of create and edit product form

--- backoffice/modules/catalog/components/ProductGeneralInformation.tsx
+++ backoffice/modules/catalog/components/ProductGeneralInformation.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
-import { Check, Input, Text } from '../../../common/items/Input';
-import { Brand } from '../models/Brand';
-import { getBrands } from '../services/BrandService';
-import { UseFormRegister, FieldErrorsImpl, UseFormSetValue } from 'react-hook-form';
-import { ProductPost } from '../models/ProductPost';
+import { FieldErrorsImpl, UseFormRegister, UseFormSetValue } from 'react-hook-form';
 import slugify from 'slugify';
+
+import { Check, Input, Text } from '../../../common/items/Input';
+import Select from '../../../common/items/Select';
+import { Brand } from '../models/Brand';
+import { ProductPost } from '../models/ProductPost';
+import { getBrands } from '../services/BrandService';
 
 type Props = {
   register: UseFormRegister<ProductPost>;
@@ -14,27 +16,25 @@ type Props = {
 
 const ProductGeneralInformation = ({ register, errors, setValue }: Props) => {
   const [brands, setBrands] = useState<Brand[]>([]);
+
   useEffect(() => {
     getBrands().then((data) => {
       setBrands(data);
     });
   }, []);
+
   return (
     <>
-      <div className="mb-3">
-        <label className="form-label" htmlFor="product-name">
-          Product Name
-        </label>
-        <input
-          type="text"
-          id="product-name"
-          className={`form-control ${errors.name?.message ? 'border-danger' : ''}`}
-          {...register('name', {
-            onChange: (event) => setValue('slug', slugify(event.target.value)),
-          })}
-        />
-        <sup className="text-danger fst-italic">{errors.name?.message}</sup>
-      </div>
+      <Input
+        labelText="Product name"
+        field="name"
+        register={register}
+        registerOptions={{
+          required: { value: true, message: 'Product name is required' },
+          onChange: (event) => setValue('slug', slugify(event.target.value, { lower: true })),
+        }}
+        error={errors.name?.message}
+      />
       <Input labelText="Slug" field="slug" register={register} error={errors.slug?.message} />
       <Input labelText="SKU" field="sku" register={register} error={errors.sku?.message} />
       <Input labelText="GTIN" field="gtin" register={register} error={errors.gtin?.message} />
@@ -62,22 +62,18 @@ const ProductGeneralInformation = ({ register, errors, setValue }: Props) => {
         register={register}
         error={errors.price?.message}
         type="number"
+        registerOptions={{ min: { value: 0, message: 'Price must be greater than 0' } }}
       />
-      <div className="mb-3">
-        <label className="form-label" htmlFor="brand">
-          Brand
-        </label>
-        <select {...register('brandId')} id="brand" className="form-control">
-          <option value="0" hidden>
-            Select Brand
-          </option>
-          {(brands || []).map((brand) => (
-            <option value={brand.id} key={brand.id}>
-              {brand.name}
-            </option>
-          ))}
-        </select>
-      </div>
+
+      <Select
+        labelText="Brand"
+        field="brandId"
+        placeholder="Select brand"
+        options={brands}
+        register={register}
+        registerOptions={{ required: { value: true, message: 'Please select brand' } }}
+        error={errors.brandId?.message}
+      />
 
       <Check labelText="Is Allowed To Order" field="isAllowedToOrder" register={register} />
       <Check labelText="Is Published" field="isPublished" register={register} />
