ae4cc70f58b0017025d0a8118f505cd5b78b51b0
Thien Nguyen <thienn@outlook.com>
2023-02-12 20:24:06 +0700

    override zipkin endpoint

--- backoffice-bff/src/main/java/com/yas/backofficebff/Application.java
+++ backoffice-bff/src/main/java/com/yas/backofficebff/Application.java
@@ -1,7 +1,9 @@
 package com.yas.backofficebff;
 
+import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
@@ -10,6 +12,13 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ZipkinSpanExporter zipkinSpanExporter() {
+		return ZipkinSpanExporter.builder()
+				.setEndpoint("http://tempo:9411/api/v2/spans")
+				.build();
 	}
 
 }
