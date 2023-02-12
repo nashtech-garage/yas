1a19567d5748c8456141d66e2f3336d44353fec5
Thien Nguyen <thienn@outlook.com>
2023-02-12 08:54:10 +0700

    code clean up

--- product/src/main/java/com/yas/product/viewmodel/ProductAttributeValuePostVm.java
+++ product/src/main/java/com/yas/product/viewmodel/ProductAttributeValuePostVm.java
@@ -1,6 +1,4 @@
 package com.yas.product.viewmodel;
 
-import jakarta.validation.constraints.NotEmpty;
-
 public record ProductAttributeValuePostVm( Long ProductId , Long productAttributeId, String value) {
 }
