0dfff5373c3310e9c8a7ddf4da214b2ee7addec3
Thien Nguyen <thienn@outlook.com>
2023-02-14 06:03:27 +0700

    instrument otel for storefront

--- storefront-bff/src/main/java/com/yas/storefrontbff/StorefrontBffApplication.java
+++ storefront-bff/src/main/java/com/yas/storefrontbff/StorefrontBffApplication.java
@@ -1,9 +1,11 @@
 package com.yas.storefrontbff;
 
 import com.yas.storefrontbff.config.ServiceUrlConfig;
+import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
@@ -13,6 +15,14 @@ public class StorefrontBffApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StorefrontBffApplication.class, args);
+	}
+
+	//TODO move the zipkin endpoint to config file. Consider using Autoconfigure when it ready (alpha at the moment)
+	@Bean
+	public ZipkinSpanExporter zipkinSpanExporter() {
+		return ZipkinSpanExporter.builder()
+				.setEndpoint("http://tempo:9411/api/v2/spans")
+				.build();
 	}
 
 }
