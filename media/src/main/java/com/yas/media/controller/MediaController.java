f949f2dbf2a25ccba55e1e9f402ebfd15919193a
Thien Nguyen Le Quynh <nlqthien@gmail.com>
2023-02-14 06:45:06 +0700

    upgrade media to spring boot 3 and otel (#262)

--- media/src/main/java/com/yas/media/controller/MediaController.java
+++ media/src/main/java/com/yas/media/controller/MediaController.java
@@ -16,7 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
--- media/src/main/java/com/yas/media/model/Media.java
+++ media/src/main/java/com/yas/media/model/Media.java
@@ -3,7 +3,7 @@ package com.yas.media.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Getter
 @Setter
