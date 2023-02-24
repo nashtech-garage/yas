402262c8ffce401b23bcc3929022635d6729ad06
LEHOANGGLAM <lelam1384@gmail.com>
2023-02-24 13:30:42 +0700

    remove duplicate column customerId in rating model

--- rating/src/main/java/com/yas/rating/viewmodel/RatingPostVm.java
+++ rating/src/main/java/com/yas/rating/viewmodel/RatingPostVm.java
@@ -3,7 +3,7 @@ package com.yas.rating.viewmodel;
 import com.yas.rating.model.Rating;
 
 public record RatingPostVm(
-        String content, int star, String customerId, Long productId
+        String content, int star, Long productId
 ) {
 
 }
