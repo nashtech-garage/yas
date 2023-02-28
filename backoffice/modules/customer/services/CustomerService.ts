101403a6e4ce4613eae4eecf51a326338286fb96
Khanh Tran <khanh.tranduy@nashtechglobal.com>
2023-02-28 11:24:37 +0700

    Get Customer service returns 404

--- backoffice/modules/customer/services/CustomerService.ts
+++ backoffice/modules/customer/services/CustomerService.ts
@@ -1,6 +1,6 @@
 import { Customers } from '../models/Customers';
 
 export async function getCustomers(pageNo: number): Promise<Customers> {
-  const response = await fetch(`/api/customer/backoffice/customers/?pageNo=${pageNo}`);
+  const response = await fetch(`/api/customer/backoffice/customers?pageNo=${pageNo}`);
   return response.json();
 }
