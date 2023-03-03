10affcd53de32eb7b19b9d2c1989a13e9f072410
nhientrand <116697710+nhientrand@users.noreply.github.com>
2023-03-04 06:29:14 +0700

    Fix sonar issue and refactor code (#307)
    Co-authored-by: nhientrand <nhien.tranduc@nashtechglobal.com>

--- rating/src/main/java/com/yas/rating/service/RatingService.java
+++ rating/src/main/java/com/yas/rating/service/RatingService.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
