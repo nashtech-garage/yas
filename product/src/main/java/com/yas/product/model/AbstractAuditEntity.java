3e4bb0dee070f4448fad64450ad98e41f65345a4
Dinh Thai <dinhthaivv@nashtechglobal.com>
2023-03-07 14:10:04 +0700

    #322 auto set createdBy and lastModifiedBy

--- product/src/main/java/com/yas/product/model/AbstractAuditEntity.java
+++ product/src/main/java/com/yas/product/model/AbstractAuditEntity.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
 import java.time.ZonedDateTime;
 
 @MappedSuperclass
@@ -12,9 +15,11 @@ import java.time.ZonedDateTime;
 public class AbstractAuditEntity {
     private ZonedDateTime createdOn = ZonedDateTime.now();
 
+    @CreatedBy
     private String createdBy;
 
     private ZonedDateTime lastModifiedOn = ZonedDateTime.now();
 
+    @LastModifiedBy
     private String lastModifiedBy;
 }
