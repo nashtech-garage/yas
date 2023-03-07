a7b9df3d5f72c1db5f33432eac868dcd9b5e5a13
chanhnguyen6975 <95927642+chanhnguyen6975@users.noreply.github.com>
2023-03-07 18:29:32 +0700

    #297 update brand pages and apply slug pattern (#323)
    * #297 update brand pages and apply slug pattern
    
    * remove some unused libs
    
    * remove unused libs
    
    * fix some comments
    
    * format code
    
    ---------
    
    Co-authored-by: Chanh Nguyen Trung <chanh.nguyentrung@nashtechglobal.com>

---
+++ backoffice/modules/catalog/components/BrandGeneralInformation.tsx
@@ -0,0 +1,60 @@
+import { useRouter } from 'next/router';
+import { FieldErrorsImpl, UseFormRegister, UseFormSetValue, UseFormTrigger } from 'react-hook-form';
+import slugify from 'slugify';
+
+import { Input } from '../../../common/items/Input';
+import { Brand } from '../models/Brand';
+import { SLUG_FIELD_PATTERN } from '../constants/validationPattern';
+
+type Props = {
+  register: UseFormRegister<Brand>;
+  errors: FieldErrorsImpl<Brand>;
+  setValue: UseFormSetValue<Brand>;
+  trigger: UseFormTrigger<Brand>;
+  brand?: Brand;
+};
+
+const BrandGeneralInformation = ({ register, errors, setValue, trigger, brand }: Props) => {
+  const router = useRouter();
+  const onNameChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue('slug', slugify(event.target.value, { lower: true, strict: true }));
+    await trigger('slug');
+    await trigger('name');
+  };
+  if (!brand) {
+    return <></>;
+  } else {
+    return (
+      <>
+        <Input
+          labelText="Name"
+          field="name"
+          defaultValue={brand?.name}
+          register={register}
+          registerOptions={{
+            required: { value: true, message: 'Brand name is required' },
+            onChange: onNameChange,
+          }}
+          error={errors.name?.message}
+        />
+        <Input
+          labelText="Slug"
+          field="slug"
+          defaultValue={brand?.slug}
+          register={register}
+          registerOptions={{
+            required: { value: true, message: 'Slug brand is required' },
+            pattern: {
+              value: SLUG_FIELD_PATTERN,
+              message:
+                'Slug must not contain special characters except dash and all characters must be lowercase',
+            },
+          }}
+          error={errors.slug?.message}
+        />
+      </>
+    );
+  }
+};
+
+export default BrandGeneralInformation;
--- backoffice/pages/catalog/brands/create.tsx
+++ backoffice/pages/catalog/brands/create.tsx
@@ -1,66 +1,43 @@
 import type { NextPage } from 'next';
 import { Brand } from '../../../modules/catalog/models/Brand';
 import { createBrand } from '../../../modules/catalog/services/BrandService';
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import BrandGeneralInformation from '../../../modules/catalog/components/BrandGeneralInformation';
 
 const BrandCreate: NextPage = () => {
-  const { register, handleSubmit, formState } = useForm();
-  const { errors } = formState;
-  const [generateSlug, setGenerateSlug] = useState<string>();
-  const handleSubmitBrand = async (event: any) => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    trigger,
+  } = useForm<Brand>();
+  const handleSubmitBrand = async (event: Brand) => {
     let brand: Brand = {
       id: 0,
       name: event.name,
-      slug: generateSlug === undefined ? '' : generateSlug,
+      slug: event.slug,
     };
     brand = await createBrand(brand);
-    location.replace('/catalog/brands');
+    router.replace('/catalog/brands');
   };
-  const slugify = require('slugify');
-  const onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setGenerateSlug(slugify(event.target.value));
-  };
-  const onSlugChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setGenerateSlug(event.target.value);
-  };
+
   return (
     <>
       <div className="row mt-5">
         <div className="col-md-8">
           <h2>Create brand</h2>
           <form onSubmit={handleSubmit(handleSubmitBrand)}>
-            <div className="mb-3">
-              <div className="mb-3">
-                <label className="form-label" htmlFor="slug">
-                  Name
-                </label>
-                <input
-                  className="form-control"
-                  {...register('name', { required: true, onChange: onNameChange })}
-                  type="text"
-                  id="name"
-                  name="name"
-                />
-                {errors.name && errors.name.type == 'required' && (
-                  <p className="text-danger">Please enter the name brand</p>
-                )}
-              </div>
-            </div>
-            <div className="mb-3">
-              <label className="form-label" htmlFor="slug">
-                Slug
-              </label>
-              <input
-                className="form-control"
-                type="text"
-                id="slug"
-                defaultValue={generateSlug}
-                name="slug"
-                onChange={onSlugChange}
-              />
-            </div>
+            <BrandGeneralInformation
+              register={register}
+              errors={errors}
+              setValue={setValue}
+              trigger={trigger}
+            />
             <button className="btn btn-primary" type="submit">
               Save
             </button>
