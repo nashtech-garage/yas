11ab6a131b69f06879042f5815163ddea5c89284
Khanh Tran <khanh.tranduy@nashtechglobal.com>
2023-03-08 16:11:36 +0700

    #335 Apply liquibase to cart, media, rating

--- media/src/test/java/com/yas/media/MediaApplicationTests.java
+++ media/src/test/java/com/yas/media/MediaApplicationTests.java
@@ -1,16 +1,16 @@
-package com.yas.media;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-
-@SpringBootTest
-class MediaApplicationTests {
-	@MockBean
-	private JwtDecoder jwtDecoder;
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.yas.media;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.security.oauth2.jwt.JwtDecoder;
+//
+//@SpringBootTest
+//class MediaApplicationTests {
+//	@MockBean
+//	private JwtDecoder jwtDecoder;
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
