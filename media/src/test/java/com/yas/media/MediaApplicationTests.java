20bbf28782d31bdaaad231275b5013d9ccb0eabe
Khanh Tran <khanh.tranduy@nashtechglobal.com>
2023-03-08 16:24:07 +0700

    #335 Fix unit test

--- media/src/test/java/com/yas/media/MediaApplicationTests.java
+++ media/src/test/java/com/yas/media/MediaApplicationTests.java
@@ -1,16 +1,14 @@
-//package com.yas.media;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.security.oauth2.jwt.JwtDecoder;
-//
-//@SpringBootTest
-//class MediaApplicationTests {
-//	@MockBean
-//	private JwtDecoder jwtDecoder;
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.yas.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
+class MediaApplicationTests {
+	@MockBean
+	private JwtDecoder jwtDecoder;
+	@Test
+	void contextLoads() {
+	}
+
+}
