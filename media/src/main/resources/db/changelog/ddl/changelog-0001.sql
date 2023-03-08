afc648e36f55064b839d1b7af3005fc1e8235ef1
Khanh Tran <khanh.tranduy@nashtechglobal.com>
2023-03-08 14:52:56 +0700

    #335 Apply liquibase to cart, media, rating

--- media/src/main/resources/db/changelog/ddl/changelog-0001.sql
+++ media/src/main/resources/db/changelog/ddl/changelog-0001.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 
 --changeset khanhtran:issue-335-1
-create table media (id bigserial not null, caption varchar(255), data oid, file_name varchar(255), media_type varchar(255), primary key (id));
+create table media (id bigserial not null, caption varchar(255), data bytea, file_name varchar(255), media_type varchar(255), primary key (id));
