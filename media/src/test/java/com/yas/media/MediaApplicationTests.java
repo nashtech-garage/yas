5059624a44a64b05f7067f24a83c169c70fb5b62
khanhtrand <khanhtrand141@gmail.com>
2023-03-08 16:27:35 +0700

    Issue/#335 Apply liquibase (#341)
    * #335 Apply liquibase to product
    
    * #335 Apply liquibase to cart, media, rating
    
    * #335 Apply liquibase to cart, media, rating
    
    * #335 Apply liquibase to cart, media, rating
    
    * #335 Fix unit test
    
    ---------
    
    Co-authored-by: Khanh Tran <khanh.tranduy@nashtechglobal.com>

--- media/src/test/java/com/yas/media/MediaApplicationTests.java
+++ media/src/test/java/com/yas/media/MediaApplicationTests.java
@@ -1,11 +1,9 @@
 package com.yas.media;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
-@SpringBootTest
 class MediaApplicationTests {
 	@MockBean
 	private JwtDecoder jwtDecoder;
