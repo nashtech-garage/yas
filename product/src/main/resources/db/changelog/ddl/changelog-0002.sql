f1a6237b4a812c39d2469287c8202a4511eee5a2
lnghia <trongnghialedinh@gmail.com>
2023-03-10 14:13:20 +0700

    #352 change syntax

--- product/src/main/resources/db/changelog/ddl/changelog-0002.sql
+++ product/src/main/resources/db/changelog/ddl/changelog-0002.sql
@@ -2,5 +2,8 @@
 
 --changeset lnghia:issue-352-1
 alter table if exists product
-    alter column description type text,
+    alter column description type text;
+
+--changeset lnghia:issue-352-2
+alter table if exists product
     alter column specification type text;
