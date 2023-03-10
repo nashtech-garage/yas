8c1c291afcb3243e87af963192a98a11f20de009
HARVEYNASH\dieu.nguyenthi <dieu.nguyenthi@nastechglobal.com>
2023-03-10 09:30:14 +0700

    refactor

--- backoffice/common/components/ToastContext.tsx
+++ backoffice/common/components/ToastContext.tsx
@@ -28,7 +28,7 @@ const router = useRouter();
   const [toastHeader, setToastHeader] = useState();
   const [toastVariant, setToastVariant] = useState('error');
 
-  const setToastProperties = (header : string, variant : string, show : boolean) => {
+  const setToastProperties = (header : any, variant : string, show : boolean) => {
     setShowToast(show);
     setToastHeader(header);
     setToastVariant(variant);
