ba20320685247507c7c47291c82d10c830ae06d2
HARVEYNASH\dieu.nguyenthi <dieu.nguyenthi@nastechglobal.com>
2023-03-16 16:56:09 +0700

    no message

--- backoffice/modules/catalog/services/ResponseStatusHandlingService.ts
+++ backoffice/modules/catalog/services/ResponseStatusHandlingService.ts
@@ -1,4 +1,3 @@
-
 import { toastSuccess, toastError } from './ToastService';
 
 import {
@@ -13,57 +12,52 @@ import {
   ResponseTitle,
 } from '../../../constants/Common';
 
-
-
 //Handle deleting response message from API
-  export const handleDeletingResponse = (response: any, itemName: string | number) => {
-
-    if (response.status === ResponseStatus.SUCCESS) {
+export const handleDeletingResponse = (response: any, itemName: string | number) => {
+  if (response.status === ResponseStatus.SUCCESS) {
     toastSuccess(itemName + HAVE_BEEN_DELETED);
-      //setToastProperties(itemName + HAVE_BEEN_DELETED, ToastVariant.SUCCESS, true);
-    } else if (response.title === ResponseTitle.NOT_FOUND) {
+    //setToastProperties(itemName + HAVE_BEEN_DELETED, ToastVariant.SUCCESS, true);
+  } else if (response.title === ResponseTitle.NOT_FOUND) {
     toastError(response.detail);
-      //setToastProperties(response.detail, ToastVariant.ERROR, true);
-    } else if (response.title === ResponseTitle.BAD_REQUEST) {
+    //setToastProperties(response.detail, ToastVariant.ERROR, true);
+  } else if (response.title === ResponseTitle.BAD_REQUEST) {
     toastError(response.detail);
-      //setToastProperties(response.detail, ToastVariant.ERROR, true);
-    } else {
+    //setToastProperties(response.detail, ToastVariant.ERROR, true);
+  } else {
     toastError(DELETE_FAILED);
-      //setToastProperties(DELETE_FAILED, ToastVariant.ERROR, true);
-    }
-  };
+    //setToastProperties(DELETE_FAILED, ToastVariant.ERROR, true);
+  }
+};
 
-  //Handle updating response message from API
-  export const handleUpdatingResponse = (response: any) => {
-
-    if (response.status === ResponseStatus.SUCCESS) {
-      toastSuccess(UPDATE_SUCCESSFULLY);
-      //useRouter().replace(url);
-    } else if (response.title === ResponseTitle.NOT_FOUND) {
-      toastError(response.detail);
-     // useRouter().replace(url);
-    } else if (response.title === ResponseTitle.BAD_REQUEST) {
-      toastError(response.detail);
-    } else {
-      toastError(UPDATE_FAILED);
+//Handle updating response message from API
+export const handleUpdatingResponse = (response: any) => {
+  if (response.status === ResponseStatus.SUCCESS) {
+    toastSuccess(UPDATE_SUCCESSFULLY);
+    //useRouter().replace(url);
+  } else if (response.title === ResponseTitle.NOT_FOUND) {
+    toastError(response.detail);
+    // useRouter().replace(url);
+  } else if (response.title === ResponseTitle.BAD_REQUEST) {
+    toastError(response.detail);
+  } else {
+    toastError(UPDATE_FAILED);
     //  useRouter().replace(url);
-    }
-  };
+  }
+};
 
-  /*Handle creating response message from API
-   * setup info global for toast
-   *
-   */
-  export const handleCreatingResponse = async (response: any) => {
-
-    if (response.status === ResponseStatus.CREATED) {
-      toastSuccess(CREATE_SUCCESSFULLY);
-     // useRouter().replace(url);
-    } else if (response.status === ResponseStatus.BAD_REQUEST) {
-      response = await response.json();
-      toastError(response.detail);
-    } else {
-      toastError(CREATE_FAILED);
-     //useRouter().replace(url);
-    }
-  };
+/*Handle creating response message from API
+ * setup info global for toast
+ *
+ */
+export const handleCreatingResponse = async (response: any) => {
+  if (response.status === ResponseStatus.CREATED) {
+    toastSuccess(CREATE_SUCCESSFULLY);
+    // useRouter().replace(url);
+  } else if (response.status === ResponseStatus.BAD_REQUEST) {
+    response = await response.json();
+    toastError(response.detail);
+  } else {
+    toastError(CREATE_FAILED);
+    //useRouter().replace(url);
+  }
+};
