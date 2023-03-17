184b75688244fbb198b7739a91f27f72da69ef82
LEHOANGGLAM <lelam1384@gmail.com>
2023-03-17 16:12:00 +0700

    fix sonar build fail v4

--- order/src/main/java/com/yas/order/config/CorsConfig.java
+++ order/src/main/java/com/yas/order/config/CorsConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigure() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigure() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*")
+//                        .allowedHeaders("*");
+//            }
+//        };
+//    }
 }
