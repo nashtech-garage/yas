e8db9c1d43b1a45782c9eac35002d31346932c8b
HARVEYNASH\dieu.nguyenthi <dieu.nguyenthi@nastechglobal.com>
2023-03-17 13:11:18 +0700

    no message

--- backoffice/pages/catalog/product-attribute-groups/[id]/edit.tsx
+++ backoffice/pages/catalog/product-attribute-groups/[id]/edit.tsx
@@ -11,7 +11,6 @@ import {
 import { PRODUCT_ATTRIBUTE_GROUPS_URL } from '../../../../constants/Common';
 import { handleUpdatingResponse } from '../../../../modules/catalog/services/ResponseStatusHandlingService';
 import ProductAttributeGroupGeneralInformation from '../../../../modules/catalog/components/ProductAttributeGroupGeneralInformation';
-import CustomToast from '../../../../common/items/CustomToast';
 
 const ProductAttributeGroupEdit: NextPage = () => {
   const router = useRouter();
