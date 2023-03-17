4b04d274936f95bd914b5a9df3eb1ef82028e912
HARVEYNASH\dieu.nguyenthi <dieu.nguyenthi@nastechglobal.com>
2023-03-17 09:30:15 +0700

    no message

--- backoffice/modules/catalog/services/ResponseStatusHandlingService.ts
+++ backoffice/modules/catalog/services/ResponseStatusHandlingService.ts
@@ -16,16 +16,12 @@ import {
 export const handleDeletingResponse = (response: any, itemName: string | number) => {
   if (response.status === ResponseStatus.SUCCESS) {
     toastSuccess(itemName + HAVE_BEEN_DELETED);
-    //setToastProperties(itemName + HAVE_BEEN_DELETED, ToastVariant.SUCCESS, true);
   } else if (response.title === ResponseTitle.NOT_FOUND) {
     toastError(response.detail);
-    //setToastProperties(response.detail, ToastVariant.ERROR, true);
   } else if (response.title === ResponseTitle.BAD_REQUEST) {
     toastError(response.detail);
-    //setToastProperties(response.detail, ToastVariant.ERROR, true);
   } else {
     toastError(DELETE_FAILED);
-    //setToastProperties(DELETE_FAILED, ToastVariant.ERROR, true);
   }
 };
 
@@ -33,15 +29,12 @@ export const handleDeletingResponse = (response: any, itemName: string | number)
 export const handleUpdatingResponse = (response: any) => {
   if (response.status === ResponseStatus.SUCCESS) {
     toastSuccess(UPDATE_SUCCESSFULLY);
-    //useRouter().replace(url);
   } else if (response.title === ResponseTitle.NOT_FOUND) {
     toastError(response.detail);
-    // useRouter().replace(url);
   } else if (response.title === ResponseTitle.BAD_REQUEST) {
     toastError(response.detail);
   } else {
     toastError(UPDATE_FAILED);
-    //  useRouter().replace(url);
   }
 };
 
@@ -52,12 +45,10 @@ export const handleUpdatingResponse = (response: any) => {
 export const handleCreatingResponse = async (response: any) => {
   if (response.status === ResponseStatus.CREATED) {
     toastSuccess(CREATE_SUCCESSFULLY);
-    // useRouter().replace(url);
   } else if (response.status === ResponseStatus.BAD_REQUEST) {
     response = await response.json();
     toastError(response.detail);
   } else {
     toastError(CREATE_FAILED);
-    //useRouter().replace(url);
   }
 };
