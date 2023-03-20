805eebd3b486970af64effb86cf12dbc2d97685d
LEHOANGGLAM <lelam1384@gmail.com>
2023-03-20 10:46:11 +0700

    //NOSONAR v2

--- order/src/main/java/com/yas/order/config/CorsConfig.java
+++ order/src/main/java/com/yas/order/config/CorsConfig.java
@@ -12,8 +12,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*")
-                        .allowedHeaders("*"); //NOSONAR
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedHeaders("*"); //NOSONAR
             }
         };
     }
