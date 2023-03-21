e756d2e557c2ccd109e35f34aed9170ca1b5c724
chanhnguyen6975 <95927642+chanhnguyen6975@users.noreply.github.com>
2023-03-21 13:05:47 +0700

    lowercase slug all pages (#471)
    * lowercase slug
    
    * Update create.tsx
    
    ---------
    
    Co-authored-by: Chanh Nguyen Trung <chanh.nguyentrung@nashtechglobal.com>

--- backoffice/pages/catalog/categories/create.tsx
+++ backoffice/pages/catalog/categories/create.tsx
@@ -74,12 +74,8 @@ const CategoryCreate: NextPage = () => {
                   slug?.setAttribute(
                     'value',
                     slugify(e.target.value, {
-                      replacement: '-', // replace spaces with replacement character, defaults to `-`
-                      remove: undefined, // remove characters that match regex, defaults to `undefined`
-                      lower: true, // convert to lower case, defaults to `false`
-                      strict: false, // strip special characters except replacement, defaults to `false`
-                      locale: 'vi', // language code of the locale to use
-                      trim: true, // trim leading and trailing replacement chars, defaults to `true`
+                      lower: true,
+                      strict: true,
                     })
                   );
                 }}
