83b664b6b3ebd82bc1a972f500cc3b4b03ece6f8
LEHOANGGLAM <lelam1384@gmail.com>
2023-03-21 13:00:44 +0700

    handle search and delete in reviews page

---
+++ rating/src/main/java/com/yas/rating/viewmodel/ResponeStatusVm.java
@@ -0,0 +1,9 @@
+package com.yas.rating.viewmodel;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record ResponeStatusVm(String title, String message, String statusCode){
+}
