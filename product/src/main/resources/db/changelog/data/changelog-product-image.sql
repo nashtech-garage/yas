cfc72e63030c697e29101b9e3c0494cf911851ae
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2023-03-21 10:32:44 +0700

    move insert image product to product sql

--- product/src/main/resources/db/changelog/data/changelog-product-image.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset nguyenvanhadncntt:issue-393-1
-INSERT INTO product_image (image_id,product_id) VALUES (2,1), (4,2);
-update product set thumbnail_media_id = 1 where id = 1;
-update product set thumbnail_media_id = 3 where id = 2;
-
