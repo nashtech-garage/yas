2ec8026fb5bfc0e5bca93153685702e172d95290
Hoàng Lâm <62151844+LEHOANGGLAM@users.noreply.github.com>
2023-03-21 15:09:10 +0700

    handle search and delete in reviews page (#478)
    * handle search and delete in reviews page
    
    * messs constants

--- rating/src/main/java/com/yas/rating/controller/RatingController.java
+++ rating/src/main/java/com/yas/rating/controller/RatingController.java
@@ -4,6 +4,7 @@ import com.yas.rating.service.RatingService;
 import com.yas.rating.viewmodel.RatingListVm;
 import com.yas.rating.viewmodel.RatingPostVm;
 import com.yas.rating.viewmodel.RatingVm;
+import com.yas.rating.viewmodel.ResponeStatusVm;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,23 @@ public class RatingController {
     public RatingController(RatingService ratingService) {
         this.ratingService = ratingService;
     }
-    @GetMapping({"/storefront/ratings/products/{productId}", "/backoffice/ratings/products/{productId}"})
+
+    @GetMapping("/backoffice/ratings/products/{productId}")
+    public ResponseEntity<RatingListVm> getRatingListWithFilter(
+            @PathVariable Long productId,
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "5", required = false) int pageSize,
+            @RequestParam(value = "name", defaultValue = "", required = false) String name) {
+
+        return ResponseEntity.ok(ratingService.getRatingListByProductIdAndCustomerName(productId, name, pageNo, pageSize));
+    }
+
+    @DeleteMapping("/backoffice/ratings/{id}")
+    public ResponseEntity<ResponeStatusVm> deleteRating(@PathVariable Long id) {
+        return ResponseEntity.ok(ratingService.deleteRating(id));
+    }
+
+    @GetMapping({"/storefront/ratings/products/{productId}"})
     public ResponseEntity<RatingListVm> getRatingList(
             @PathVariable Long productId,
             @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
@@ -25,12 +42,12 @@ public class RatingController {
     }
 
     @PostMapping("/storefront/ratings")
-    public ResponseEntity<RatingVm> createRating(@Valid @RequestBody  RatingPostVm ratingPostVm) {
+    public ResponseEntity<RatingVm> createRating(@Valid @RequestBody RatingPostVm ratingPostVm) {
         return ResponseEntity.ok(ratingService.createRating(ratingPostVm));
     }
 
     @GetMapping("/storefront/ratings/product/{productId}/average-star")
-    public Double getAverageStarOfProduct( @PathVariable Long productId) {
+    public Double getAverageStarOfProduct(@PathVariable Long productId) {
         return ratingService.calculateAverageStar(productId);
     }
 }
--- rating/src/main/java/com/yas/rating/service/RatingService.java
+++ rating/src/main/java/com/yas/rating/service/RatingService.java
@@ -5,15 +5,13 @@ import com.yas.rating.exception.NotFoundException;
 import com.yas.rating.model.Rating;
 import com.yas.rating.repository.RatingRepository;
 import com.yas.rating.utils.Constants;
-import com.yas.rating.viewmodel.CustomerVm;
-import com.yas.rating.viewmodel.RatingListVm;
-import com.yas.rating.viewmodel.RatingPostVm;
-import com.yas.rating.viewmodel.RatingVm;
+import com.yas.rating.viewmodel.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
@@ -45,6 +43,18 @@ public class RatingService {
         return new RatingListVm(ratingVmList, ratings.getTotalElements(), ratings.getTotalPages());
     }
 
+    public RatingListVm getRatingListByProductIdAndCustomerName(Long id, String name, int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by("createdOn").descending());
+        Page<Rating> ratings = ratingRepository.findByProductIdAndCustomerName(id, name.toLowerCase(), pageable);
+
+        List<RatingVm> ratingVmList = new ArrayList<>();
+        for (Rating rating : ratings.getContent()) {
+            ratingVmList.add(RatingVm.fromModel(rating));
+        }
+
+        return new RatingListVm(ratingVmList, ratings.getTotalElements(), ratings.getTotalPages());
+    }
+
     public RatingVm createRating(RatingPostVm ratingPostVm) {
         Rating rating = new Rating();
         rating.setRatingStar(ratingPostVm.star());
@@ -59,9 +69,17 @@ public class RatingService {
         return RatingVm.fromModel(savedRating);
     }
 
-    public Double calculateAverageStar(Long productId){
+    public ResponeStatusVm deleteRating(long ratingId) {
+        Rating rating = ratingRepository.findById(ratingId)
+                .orElseThrow(() -> new NotFoundException(Constants.ERROR_CODE.RATING_NOT_FOUND, ratingId));
+
+        ratingRepository.delete(rating);
+        return new ResponeStatusVm("Delete Rating", Constants.MESSAGE.SUCCESS_MESSAGE, HttpStatus.OK.toString());
+    }
+
+    public Double calculateAverageStar(Long productId) {
         List<Object[]> totalStarsAndRatings = ratingRepository.getTotalStarsAndTotalRatings(productId);
-        if(ObjectUtils.isEmpty(totalStarsAndRatings.get(0)[0]))
+        if (ObjectUtils.isEmpty(totalStarsAndRatings.get(0)[0]))
             return 0.0;
         int totalStars = (Integer.parseInt(totalStarsAndRatings.get(0)[0].toString()));
         int totalRatings = (Integer.parseInt(totalStarsAndRatings.get(0)[1].toString()));
