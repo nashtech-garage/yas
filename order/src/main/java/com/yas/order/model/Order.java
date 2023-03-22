ea2bdc102427b7ca7bb3ea9e19b2d2eca5c3e9b5
LEHOANGGLAM <lelam1384@gmail.com>
2023-03-22 13:08:00 +0700

    remove liquibase

--- order/src/main/java/com/yas/order/model/Order.java
+++ order/src/main/java/com/yas/order/model/Order.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "order")
+@Table(name = "`order`")
 @Getter
 @Setter
 @NoArgsConstructor
--- order/src/main/resources/db/changelog/ddl/changelog-0001.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
-
---changeset lecamhoanglam:issue-440-1
-create table order (id bigserial not null);
