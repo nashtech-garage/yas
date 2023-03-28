758e1f971aff22bcbe09d959d6616fb0d9ad705c
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2023-03-28 16:45:04 +0700

    change method

--- search/src/main/java/com/yas/search/service/ProductService.java
+++ search/src/main/java/com/yas/search/service/ProductService.java
@@ -17,7 +17,7 @@ public class ProductService {
     }
 
     public List<Product> findProduct(String name) {
-        return productRepository.findByAttributeValuesValueOrBrandName(name, name);
+        return productRepository.findByName(name);
     }
 
     public List<Product> findProductAdvance(String keyword) {
