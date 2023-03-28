67767fe2fb19ec14ee0e1f772f4fe846b2f3bd7c
Hoàng Lâm <62151844+LEHOANGGLAM@users.noreply.github.com>
2023-03-28 09:57:37 +0700

    base config (#506)

---
+++ order/src/main/java/com/yas/order/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.yas.order.utils;
+
+public final class Constants {
+    public final class ERROR_CODE {
+        public static final String ORDER_NOT_FOUND = "ORDER_NOT_FOUND";
+
+    }
+
+    public final class MESSAGE {
+        public static final String SUCCESS_MESSAGE = "SUCCESS";
+    }
+}
