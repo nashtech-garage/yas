4f47557b752918af4d9185de534c87894eefd2c1
Trong Nghia <43461631+lnghia@users.noreply.github.com>
2023-03-29 16:07:50 +0700

    fix unit test

---
+++ search/src/test/java/com/yas/search/service/ProductServiceTest.java
@@ -0,0 +1,17 @@
+package com.yas.search.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+    @Test
+    void test() {
+        var a = 5;
+
+        assertEquals(a, 5);
+    }
+}
