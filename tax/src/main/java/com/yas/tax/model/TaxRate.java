b43ffad7340d9a179df1d548598dad5737409312
HARVEYNASH\dieu.nguyenthi <dieu.nguyenthi@nastechglobal.com>
2023-03-30 16:53:16 +0700

    Init Tax service

---
+++ tax/src/main/java/com/yas/tax/model/TaxRate.java
@@ -0,0 +1,47 @@
+package com.yas.tax.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tax_rate")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Builder
+public class TaxRate extends AbstractAuditEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private Double rate;
+
+  @Column(length = 25)
+  private String zipCode;
+
+  @Column(nullable = false)
+  private Long state_or_province_id;
+
+  @Column(nullable = false)
+  private Long country_id;
+
+  @ManyToOne
+  @JoinColumn(name = "tax_class_id", nullable = false)
+  private TaxClass taxClass;
+}
---
+++ tax/src/test/java/com/yas/location/service/TaxServiceTest.java
@@ -0,0 +1,18 @@
+package com.yas.tax.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class TaxServiceTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void Test() {
+    }
+
+}
