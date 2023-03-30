e68d483c66fa77c97e16009736ea7fb502f6d73e
HARVEYNASH\dieu.nguyenthi <dieu.nguyenthi@nastechglobal.com>
2023-03-30 09:58:01 +0700

    run prettier

--- backoffice/modules/system/components/CountryGeneralInformation.tsx
+++ backoffice/modules/system/components/CountryGeneralInformation.tsx
@@ -14,16 +14,16 @@ type Props = {
 const CountryGeneralInformation = ({ register, errors, setValue, trigger, country }: Props) => {
   return (
     <>
-         <Input
-            labelText="Code2"
-            field="code2"
-            defaultValue={country?.code2}
-            register={register}
-            registerOptions={{
-              required: { value: true, message: 'Code2 is required' },
-            }}
-            error={errors.code2?.message}
-          />
+      <Input
+        labelText="Code2"
+        field="code2"
+        defaultValue={country?.code2}
+        register={register}
+        registerOptions={{
+          required: { value: true, message: 'Code2 is required' },
+        }}
+        error={errors.code2?.message}
+      />
       <Input
         labelText="Name"
         field="name"
@@ -35,12 +35,7 @@ const CountryGeneralInformation = ({ register, errors, setValue, trigger, countr
         error={errors.name?.message}
       />
 
-      <Input
-        labelText="Code3"
-        field="code3"
-        defaultValue={country?.code3}
-        register={register}
-      />
+      <Input labelText="Code3" field="code3" defaultValue={country?.code3} register={register} />
       <CheckBox
         labelText="isBillingEnabled"
         field="isBillingEnabled"
