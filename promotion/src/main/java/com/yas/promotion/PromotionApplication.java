320b56f792964902adc8bf6b3fa1d649d63f8c1f
vonhu <vonhu.alien@gmail.com>
2023-03-31 09:43:25 +0700

    #546 - add serviceurl

--- promotion/src/main/java/com/yas/promotion/PromotionApplication.java
+++ promotion/src/main/java/com/yas/promotion/PromotionApplication.java
@@ -1,11 +1,14 @@
 package com.yas.promotion;
 
+import com.yas.promotion.config.ServiceUrlConfig;
 import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ServiceUrlConfig.class)
 public class PromotionApplication {
 
 	public static void main(String[] args) {
--- promotion/src/test/java/com/yas/promotion/PromotionApplicationTests.java
+++ promotion/src/test/java/com/yas/promotion/PromotionApplicationTests.java
@@ -3,10 +3,11 @@ package com.yas.promotion;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
 
 public class PromotionApplicationTests {
-
 	@BeforeEach
 	void setUp() {
 
