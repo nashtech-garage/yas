ad916972a8a96c0812602f4f67c04dceb56e17de
vonhu <vonhu.alien@gmail.com>
2023-03-31 09:20:44 +0700

    #546 - init promotion service

---
+++ promotion/src/main/java/com/yas/promotion/PromotionApplication.java
@@ -0,0 +1,21 @@
+package com.yas.promotion;
+
+import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PromotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionApplication.class, args);
+	}
+
+	@Bean
+	public ZipkinSpanExporter zipkinSpanExporter() {
+		return ZipkinSpanExporter.builder()
+				.setEndpoint("http://tempo:9411/api/v2/spans")
+				.build();
+	}
+}
---
+++ promotion/src/test/java/com/yas/promotion/PromotionApplicationTests.java
@@ -0,0 +1,19 @@
+package com.yas.promotion;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class PromotionApplicationTests {
+
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void Test() {
+	}
+
+}
