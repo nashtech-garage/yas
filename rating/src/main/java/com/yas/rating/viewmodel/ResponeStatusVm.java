0eb8a9ec8076fb09a2b75f4ef0099b51f1e745d9
Hoàng Lâm <62151844+LEHOANGGLAM@users.noreply.github.com>
2023-04-03 09:27:29 +0700

    handle code smell (#555)

--- rating/src/main/java/com/yas/rating/viewmodel/ResponeStatusVm.java
+++ rating/src/main/java/com/yas/rating/viewmodel/ResponeStatusVm.java
@@ -1,9 +1,4 @@
 package com.yas.rating.viewmodel;
 
-import org.springframework.http.HttpStatus;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public record ResponeStatusVm(String title, String message, String statusCode){
 }
