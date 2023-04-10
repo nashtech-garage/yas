49c82237b9e66d18222596edeb21ecfc11270d83
Thien Nguyen <thienn@outlook.com>
2023-04-11 06:00:35 +0700

    [product] the column is_active has removed

--- product/src/main/resources/db/changelog/data/changelog-0003-product.sql
+++ product/src/main/resources/db/changelog/data/changelog-0003-product.sql
@@ -5,7 +5,7 @@ insert into product (is_active, is_allowed_to_order,is_published,is_featured,is_
 values (true, true,true,true,true,'Dell XPS 15 9550','dell-xps-15-9550', 100000, 'CPU: 6th Gen Intel® Core™ i7 (Skylake) 6700HQ (Quad-Core, up to 3.2GHz)\nRAM: 8GB DDR4 2133MHz',1, 'The worlds smallest 15.6-inch laptop, the Dell XPS 15 stands apart with its stunning 4K UHD display and razor-thin profile. It is the only PC with a 15.6-inch')
 
 --changeset vonhu:product2
-insert into product (is_active, is_allowed_to_order,is_published,is_featured,is_visible_individually, name, slug, price, short_description, brand_id,description)
+insert into product (is_allowed_to_order,is_published,is_featured,is_visible_individually, name, slug, price, short_description, brand_id,description)
 values (true, true,true,true,true,'iPad Pro Wi-Fi 4G 128GB','ipad-pro-wi-fi-4g-128gb-gold', 100000, 'Retina Display\nATX chip\niOS 9\nApps for iPad\nSlim and light design\nRAM: 8GB DDR4 2133MHz',2, 'The worlds smallest 15.6-inch laptop, the Dell XPS 15 stands apart with its stunning 4K UHD display and razor-thin profile. It is the only PC with a 15.6-inch')
 
 --changeset nguyenvanhadncntt:issue-393-1
@@ -40,11 +40,3 @@ insert into product (name,slug,price,parent_id) values
     ('iPad Pro Wi-Fi 4G 128GB Black','ipad-pro-wi-fi-4g-128gb-black',21990000,2),
     ('iPad Pro Wi-Fi 4G 256GB Black','ipad-pro-wi-fi-4g-256gb-black',24990000,2),
     ('iPad Pro Wi-Fi 4G 512GB Black','ipad-pro-wi-fi-4g-512gb-black',28990000,2);
-
---changeset khoahd7621:issue-update-isactive-product-variant-liquibase
-update product set is_active = true where id = 3;
-update product set is_active = true where id = 4;
-update product set is_active = true where id = 5;
-update product set is_active = true where id = 6;
-update product set is_active = true where id = 7;
-update product set is_active = true where id = 8;
