92c8960557eabe3f073cec2ebd21d767cf58e4ec
HARVEYNASH\dieu.nguyenthi <dieu.nguyenthi@nastechglobal.com>
2023-04-12 18:17:53 +0700

    Change images to latest version

--- start-source-connectors.sh
+++ start-source-connectors.sh
@@ -1,2 +1,5 @@
 
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @connectors-config/connections/source.json
+curl -i -X PUT -H  "Content-Type:application/json" \
+    http://kafka-connect:8083/connectors/product-connector/config \
+    -d @connectors-config/connections/source.json
+    
