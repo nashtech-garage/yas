d7c5974acaaf70b95c88e8a45e557ad776740bcb
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-04-14 21:43:34 +0700

    Render related product list

--- storefront/modules/catalog/components/RelatedProducts.tsx
+++ storefront/modules/catalog/components/RelatedProducts.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
 
+import ProductCard from '@/common/components/ProductCard';
 import { ProductThumbnail } from '../models/ProductThumbnail';
 import { getRelatedProductsByProductId } from '../services/ProductService';
 
@@ -22,7 +24,22 @@ const RelatedProduct = ({ productId }: RelatedProductProps) => {
   };
 
   console.log('>>> Related products:', products);
-  return <div></div>;
+  return (
+    <div className="my-5">
+      <h4 className="mb-2 text-black">Related products</h4>
+      {products.length > 0 && (
+        <Row md={5}>
+          {products.map((product) => (
+            <Col key={product.id}>
+              <ProductCard product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+
+      {products.length === 0 && <p className="mt-4">No product</p>}
+    </div>
+  );
 };
 
 export default RelatedProduct;
