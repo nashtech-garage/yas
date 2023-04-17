6a44f8cb9f0fc9547f76ac0a93f4060f4ad70aa0
Kiet <118867094+kiettrangthuan@users.noreply.github.com>
2023-04-17 13:09:11 +0700

    #638 - fix bug can not create product (#644)

--- product/src/main/java/com/yas/product/repository/ProductCategoryRepository.java
+++ product/src/main/java/com/yas/product/repository/ProductCategoryRepository.java
@@ -7,7 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
     Page<ProductCategory> findAllByCategory(Pageable pageable,Category category);
+
+    List<ProductCategory> findAllByProductId(Long productId);
 }
