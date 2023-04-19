2e8399ee8ec6dc449488850c2dbc9408f796978e
LEHOANGGLAM <lelam1384@gmail.com>
2023-04-19 16:20:16 +0700

    temp

--- order/src/main/java/com/yas/order/controller/PayPalController.java
+++ order/src/main/java/com/yas/order/controller/PayPalController.java
@@ -1,2 +1,33 @@
-package com.yas.order.controller;public class PayPalController {
+package com.yas.order.controller;
+
+import com.yas.order.service.PayPalService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/paypal")
+public class PayPalController {
+
+    private final PayPalService payPalClient;
+    @Autowired
+    PayPalController(PayPalService payPalClient){
+        this.payPalClient = payPalClient;
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping(value = "/make/payment")
+    public Map<String, Object> makePayment(@RequestParam("sum") String sum){
+        return payPalClient.createPayment(sum);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping(value = "/complete/payment")
+    public Map<String, Object> completePayment(HttpServletRequest request, @RequestParam("paymentId") String paymentId, @RequestParam("payerId") String payerId){
+        return payPalClient.completePayment(request);
+    }
+
+
 }
--- order/src/main/java/com/yas/order/service/PayPalService.java
+++ order/src/main/java/com/yas/order/service/PayPalService.java
@@ -1,2 +1,92 @@
-package com.yas.order.service;public class PayPalService {
+package com.yas.order.service;
+
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class PayPalService {
+
+    String clientId = "AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS";
+    String clientSecret = "EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL";
+
+    @Autowired
+    PayPalService(){}
+
+    public Map<String, Object> createPayment(String sum){
+        Map<String, Object> response = new HashMap<String, Object>();
+        Amount amount = new Amount();
+        amount.setCurrency("USD");
+        amount.setTotal(sum);
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod("paypal");
+
+        Payment payment = new Payment();
+        payment.setIntent("sale");
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl("http://localhost:4200/cancel");
+        redirectUrls.setReturnUrl("http://localhost:4200");
+        payment.setRedirectUrls(redirectUrls);
+        Payment createdPayment;
+        try {
+            String redirectUrl = "";
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+            createdPayment = payment.create(context);
+            if(createdPayment!=null){
+                List<Links> links = createdPayment.getLinks();
+                for (Links link:links) {
+                    if(link.getRel().equals("approval_url")){
+                        redirectUrl = link.getHref();
+                        break;
+                    }
+                }
+                response.put("status", "success");
+                response.put("redirect_url", redirectUrl);
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println("Error happened during payment creation!");
+        }
+        return response;
+    }
+
+
+    public Map<String, Object> completePayment(HttpServletRequest req){
+        Map<String, Object> response = new HashMap();
+        Payment payment = new Payment();
+        payment.setId(req.getParameter("paymentId"));
+        PaymentExecution paymentExecution = new PaymentExecution();
+        paymentExecution.setPayerId(req.getParameter("payerId"));
+        try {
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+            Payment createdPayment = payment.execute(context, paymentExecution);
+            if(createdPayment!=null){
+                response.put("status", "success");
+                response.put("payment", createdPayment);
+            }
+        } catch (PayPalRESTException e) {
+            System.err.println(e.getDetails());
+        }
+        return response;
+    }
+
+
+
 }
