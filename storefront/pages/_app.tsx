54784de68fe13fa1028bdcd87cc0c39fdebd9b09
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-04-23 17:46:34 +0700

    #562 - Integrate cart context

--- storefront/pages/_app.tsx
+++ storefront/pages/_app.tsx
@@ -1,27 +1,27 @@
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ToastContainer } from 'react-toastify';
-
-import Layout from '../common/components/Layout';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 
-import '../styles/checkout.css';
-import '../styles/cart.css';
 import '../styles/Footer.css';
-import '../styles/globals.css';
 import '../styles/Header.css';
 import '../styles/HomePage.css';
+import '../styles/cart.css';
+import '../styles/checkout.css';
+import '../styles/form.css';
+import '../styles/globals.css';
 import '../styles/productDetail.css';
 import '../styles/util.css';
-import '../styles/form.css';
 
-import type { AppProps } from 'next/app';
+import Layout from '@/common/components/Layout';
+import { CartProvider } from '@/context/CartContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <CartProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </Layout>
       <ToastContainer style={{ marginTop: '70px' }} />
-    </>
+    </CartProvider>
   );
 }
 
