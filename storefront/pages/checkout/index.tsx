dcaa75574c18059b9a95f2e6c1acc5c88c04e27b
LEHOANGGLAM <lelam1384@gmail.com>
2023-05-22 10:05:51 +0700

    fix sonar build fail

--- storefront/pages/checkout/index.tsx
+++ storefront/pages/checkout/index.tsx
@@ -213,11 +213,7 @@ const Checkout = () => {
         <section className="checkout spad">
           <div className="container">
             <div className="checkout__form">
-              <form
-                onSubmit={() => {
-                  handleSubmit(onSubmitForm);
-                }}
-              >
+              <form onSubmit={handleSubmit(onSubmitForm)}>
                 <div className="row">
                   <div className="col-lg-8 col-md-6">
                     <h4>Shipping Address</h4>
