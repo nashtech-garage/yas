cb7f4aa34c714f0673736a4f5dcac98808e5fc05
Hoang Dang Khoa <hoangdangkhoa7621@gmail.com>
2023-05-30 13:08:08 +0700

    #697 - Remove pricing module (#698)

--- pricing/src/main/kotlin/com/yas/pricing/service/PriceService.kt
+++
@@ -1,8 +0,0 @@
-package com.yas.pricing.service
-
-import com.yas.pricing.domain.dto.PriceDto
-import reactor.core.publisher.Flux
-
-interface PriceService {
-    fun getTop10Price(): Flux<PriceDto>
-}
