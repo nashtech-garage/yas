37df57b849ae90e033c3dee1458cc9e0bbfcf23c
LEHOANGGLAM <lelam1384@gmail.com>
2023-06-06 17:57:59 +0700

    remove blank line

--- order/src/main/java/com/yas/order/OrderApplication.java
+++ order/src/main/java/com/yas/order/OrderApplication.java
@@ -14,14 +14,10 @@ public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
-
-
 	@Bean
 	public ZipkinSpanExporter zipkinSpanExporter() {
 		return ZipkinSpanExporter.builder()
 				.setEndpoint("http://tempo:9411/api/v2/spans")
 				.build();
 	}
-
-
 }
