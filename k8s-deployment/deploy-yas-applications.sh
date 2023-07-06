8dc5e08456e8fa8c970b7b0cadcffdcd15d77e39
nastech-bangnguyenl <98079369+nastech-bangnguyenl@users.noreply.github.com>
2023-07-06 09:48:12 +0700

    Setup k8s cluster and create helm chart deployment for yas applications (#717)

---
+++ k8s-deployment/deploy-yas-applications.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -x
+
+# Auto restart when change configmap or secret
+helm repo add stakater https://stakater.github.io/stakater-charts
+helm repo update
+
+read -rd '' DOMAIN \
+< <(yq -r '.domain' ./cluster-config.yaml)
+
+helm dependency build ../charts/yas-configuration
+helm upgrade --install yas-configuration ../charts/yas-configuration \
+--namespace yas --create-namespace
+
+sleep 20
+
+helm dependency build ../charts/backoffice-bff
+helm upgrade --install backoffice-bff ../charts/backoffice-bff \
+--namespace yas --create-namespace \
+--set backend.ingress.host="backoffice.$DOMAIN"
+
+helm dependency build ../charts/backoffice-ui
+helm upgrade --install backoffice-ui ../charts/backoffice-ui \
+--namespace yas --create-namespace
+
+sleep 60
+
+helm dependency build ../charts/storefront-bff
+helm upgrade --install storefront-bff ../charts/storefront-bff \
+--namespace yas --create-namespace \
+--set backend.ingress.host="storefront.$DOMAIN"
+
+helm dependency build ../charts/storefront-ui
+helm upgrade --install storefront-ui ../charts/storefront-ui \
+--namespace yas --create-namespace
+
+sleep 60
+
+helm upgrade --install swagger-ui ../charts/swagger-ui \
+--namespace yas --create-namespace \
+--set ingress.host="api.$DOMAIN"
+
+sleep 20
+
+for chart in {"cart","customer","inventory","location","media","order","payment","payment-paypal","product","promotion","rating","search","tax"} ; do
+    helm dependency build ../charts/"$chart"
+    helm upgrade --install "$chart" ../charts/"$chart" \
+    --namespace yas --create-namespace \
+    --set backend.ingress.host="api.$DOMAIN"
+    sleep 60
+done
