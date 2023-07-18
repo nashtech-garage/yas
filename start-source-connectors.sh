33bf60acf14ee29911c56d69250b7b8c4ef3d403
Thien Nguyen <thienn@outlook.com>
2023-07-18 09:40:59 +0700

    organize folder kafka connect

--- start-source-connectors.sh
+++ start-source-connectors.sh
@@ -1,4 +1,4 @@
 
 curl -i -X PUT -H  "Content-Type:application/json" \
     http://localhost:8083/connectors/product-connector/config \
-    -d @connectors-config/connections/source.json
+    -d @kafka/connects/debezium-product.json
