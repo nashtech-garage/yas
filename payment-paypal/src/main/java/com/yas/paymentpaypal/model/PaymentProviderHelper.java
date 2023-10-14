2094ce0f103c5e42f32fec8f19402859a8cafddd
lamlecamhoang <lam.lecamhoang@unified.com>
2023-10-14 13:17:03 +0700

    rm extra lines

--- payment-paypal/src/main/java/com/yas/paymentpaypal/model/PaymentProviderHelper.java
+++ payment-paypal/src/main/java/com/yas/paymentpaypal/model/PaymentProviderHelper.java
@@ -9,7 +9,6 @@ public class PaymentProviderHelper {
     }
     public static PaymentProviderHelper getInstance() {
         if (uniqueInstance == null) {
-            // Do the task too long before create instance ...
             // Block so other threads cannot come into while initialize
             synchronized (PaymentProviderHelper.class) {
                 // Re-check again. Maybe another thread has initialized before
