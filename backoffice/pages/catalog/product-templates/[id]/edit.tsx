3bda31850d9d941b1a315c6b07a3a731f19614ab
khanh.dinhtrong <19127437@student.hcmus.edu.vn>
2023-10-19 11:53:10 +0700

    Update handle create and edit Product Template FE

--- backoffice/pages/catalog/product-templates/[id]/edit.tsx
+++ backoffice/pages/catalog/product-templates/[id]/edit.tsx
@@ -21,7 +21,7 @@ const ProductTemplateEdit: NextPage = () => {
   let { id }: any = router.query;
   const { formState, register, handleSubmit } = useForm<FromProductTemplate>();
   const { errors } = formState;
-  const [isLoading, setLoading] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState(false);
   const [checkButton, setCheckButton] = useState<boolean>(false);
   const [productTemplate, setProductTemplate] = useState<ProductTemplate>();
   const { setValue, getValues } = useForm<FromProductTemplate>();
@@ -33,7 +33,9 @@ const ProductTemplateEdit: NextPage = () => {
     if (id !== undefined) {
       getProductAttributes().then((data) => {
         setProductAtts(data);
-      });
+      })
+      .catch((error) => console.log(error));
+
       getProductTemplate(parseInt(id)).then((data) => {
         setProductTemplate(data);
         setValue('name', data.name);
@@ -111,102 +113,99 @@ const ProductTemplateEdit: NextPage = () => {
   if (isLoading) return <p>Loading...</p>;
   if (!productTemplate) return <p>No Product Templates Id: {id}</p>;
   return (
-    <>
-      <div className="row mt-5">
-        <div className="col-md-8">
-          <h2>Edit Product Templates</h2>
-          <form onSubmit={handleSubmit(handleSubmitEditProductTemplate)}>
-            <div className="d-flex flex-column">
-              <div className="mb-4">
-                <label className="form-label" htmlFor="name">
-                  Name
-                </label>
-                <input
-                  className="form-control"
-                  {...register('name', { required: true })}
-                  type="text"
-                  id="name"
-                  name="name"
-                  defaultValue={productTemplate ? productTemplate.name : ''}
-                />
-                {errors.name && errors.name.type == 'required' && (
-                  <p className="text-danger">Please enter the name</p>
-                )}
-              </div>
-              <div className="mb-5 d-flex align-items-center">
-                <label className="form-label w-50" htmlFor="attribute">
-                  Set availability
-                </label>
-                <select
-                  className="form-control me-3"
-                  id="attribute"
-                  defaultValue="0"
-                  onChange={handleSelect}
-                >
-                  <option value="0" disabled hidden>
-                    Select Attribute
-                  </option>
-                  {(productAtts || []).map((att) => (
-                    <option value={att.name} key={att.id}>
-                      {att.name}
-                    </option>
-                  ))}
-                </select>
-                {checkButton ? (
-                  <button
-                    className="form-control w-50 btn btn-primary"
-                    style={{ height: 'auto', width: 'auto' }}
-                    onClick={onAddAttributeList}
-                  >
-                    More features
-                  </button>
-                ) : (
-                  <button
-                    className="form-control w-50 btn btn-secondary"
-                    style={{ height: 'auto', width: 'auto' }}
-                  >
-                    More features
-                  </button>
-                )}
-              </div>
-              <div className="mb-3 d-flex justify-content-evenly">
-                <label className="form-label w-50" htmlFor="attribute">
-                  Added calculations
-                </label>
-                <Table>
-                  <tbody>
-                    {Array.isArray(selectedAtts)
-                      ? selectedAtts.map((item) => (
-                          <tr key={item}>
-                            <th>{item}</th>
-                            <th style={{ textAlign: 'right' }}>
-                              <button
-                                className="btn btn-danger"
-                                onClick={(event) => onDelete(event, item)}
-                              >
-                                <i className="bi bi-x"></i>
-                              </button>
-                            </th>
-                          </tr>
-                        ))
-                      : null}
-                  </tbody>
-                </Table>
-                <div className=" w-50"></div>
-              </div>
+    <div className="row mt-5">
+      <div className="col-md-8">
+        <h2>Edit Product Templates</h2>
+        <form onSubmit={handleSubmit(handleSubmitEditProductTemplate)}>
+          <div className="d-flex flex-column">
+            <div className="mb-4">
+              <label className="form-label" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="form-control"
+                {...register('name', { required: true })}
+                type="text"
+                id="name"
+                name="name"
+                defaultValue={productTemplate ? productTemplate.name : ''}
+              />
+              {errors.name && errors.name.type == 'required' && (
+                <p className="text-danger">Please enter the name</p>
+              )}
             </div>
-            <button className="btn btn-primary" type="submit">
-              Save
+            <div className="mb-5 d-flex align-items-center">
+              <label className="form-label w-50" htmlFor="attribute">
+                Set availability
+              </label>
+              <select
+                className="form-control me-3"
+                id="attribute"
+                defaultValue="0"
+                onChange={handleSelect}
+              >
+                <option value="0" disabled hidden>
+                  Select Attribute
+                </option>
+                {(productAtts || []).map((att) => (
+                  <option value={att.name} key={att.id}>
+                    {att.name}
+                  </option>
+                ))}
+              </select>
+              {checkButton ? (
+                <button
+                  className="form-control w-50 btn btn-primary"
+                  style={{ height: 'auto', width: 'auto' }}
+                  onClick={onAddAttributeList}
+                >
+                  More features
+                </button>
+              ) : (
+                <button
+                  className="form-control w-50 btn btn-secondary"
+                  style={{ height: 'auto', width: 'auto' }}
+                >
+                  More features
+                </button>
+              )}
+            </div>
+            <div className="mb-3 d-flex justify-content-evenly">
+              <label className="form-label w-50" htmlFor="attribute">
+                Added calculations
+              </label>
+              <Table>
+                <tbody>
+                  {selectedAtts.map((item) => (
+                        <tr key={item}>
+                          <th>{item}</th>
+                          <th style={{ textAlign: 'right' }}>
+                            <button
+                              className="btn btn-danger"
+                              onClick={(event) => onDelete(event, item)}
+                            >
+                              <i className="bi bi-x"></i>
+                            </button>
+                          </th>
+                        </tr>
+                      ))
+                  }
+                </tbody>
+              </Table>
+              <div className="w-50"></div>
+            </div>
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Save
+          </button>
+          <Link href="/catalog/product-templates">
+            <button className="btn btn-danger" style={{ marginLeft: '30px' }}>
+              Cancel
             </button>
-            <Link href="/catalog/product-templates">
-              <button className="btn btn-danger" style={{ marginLeft: '30px' }}>
-                Cancel
-              </button>
-            </Link>
-          </form>
-        </div>
+          </Link>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
