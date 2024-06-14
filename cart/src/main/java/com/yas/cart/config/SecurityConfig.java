5f4e77e74c6fef60f807d5a2ce96e7f5af17a7d9
khanhtranduy <130121475+khanhtranduy@users.noreply.github.com>
2024-06-14 10:31:22 +0700

    Remove unused import classes

--- cart/src/main/java/com/yas/cart/config/SecurityConfig.java
+++ cart/src/main/java/com/yas/cart/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
