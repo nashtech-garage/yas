e9187ade3a659d5571472b8d6c79fee62abd6ffa
HARVEYNASH\minh.tranquang <minh.tranquang@nashtechglobal.com>
2024-07-18 13:42:51 +0700

    Remove webflux

--- cart/src/main/java/com/yas/cart/CartApplication.java
+++ cart/src/main/java/com/yas/cart/CartApplication.java
@@ -3,11 +3,10 @@ package com.yas.cart;
 import com.yas.cart.config.ServiceUrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(ServiceUrlConfig.class)
 @SpringBootApplication
+@EnableConfigurationProperties(ServiceUrlConfig.class)
 public class CartApplication {
 
 	public static void main(String[] args) {
