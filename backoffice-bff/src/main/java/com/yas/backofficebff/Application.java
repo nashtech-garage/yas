8d4e7257750fecb8a7be390792109718b3e65686
nashtech-huyphamphu <108394215+nashtech-huyphamphu@users.noreply.github.com>
2024-08-14 17:38:23 +0700

    #850 Fix Checkstyle BackofficeBff Module (#878)

--- backoffice-bff/src/main/java/com/yas/backofficebff/Application.java
+++ backoffice-bff/src/main/java/com/yas/backofficebff/Application.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 @EnableWebFluxSecurity
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
