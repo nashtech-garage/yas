f665e4c082b1633ff5923641c4369df5d0b1c45d
HARVEYNASH\minh.tranquang <minh.tranquang@nashtechglobal.com>
2024-08-15 17:54:45 +0700

    [Webhook] liquibase, sql script doesn't run(#861)

--- webhook/src/main/resources/db/changelog/ddl/changelog-0001.sql
+++ webhook/src/main/resources/db/changelog/ddl/changelog-0001.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+
+--changeset minhtran:issue-002
 create table webhook (id bigserial not null, payload_url text not null, content_type varchar(128), secret varchar(512), is_active bool, created_by varchar(256), created_on timestamp(6), last_modified_by varchar(255), last_modified_on timestamp(6), primary key (id));
 create table event (id bigserial not null, name varchar(512) not null, description text, primary key (id));
 create table webhook_event (id bigserial not null, webhook_id bigserial references webhook(id), event_id bigserial references event(id), primary key (id));
