aebd89fb0d48a4fa8ecc3595714edc05d7023d66
HARVEYNASH\minh.tranquang <minh.tranquang@nashtechglobal.com>
2024-08-15 14:25:10 +0700

    Add retry policy for RestClient (#841)

--- customer/src/main/java/com/yas/customer/service/AbstractCircuitBreakFallbackHandler.java
+++ customer/src/main/java/com/yas/customer/service/AbstractCircuitBreakFallbackHandler.java
@@ -5,17 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 abstract class AbstractCircuitBreakFallbackHandler {
 
-  protected void handleBodilessFallback(Throwable throwable) throws Throwable {
-    handleError(throwable);
-  }
+    protected void handleBodilessFallback(Throwable throwable) throws Throwable {
+        handleError(throwable);
+    }
 
-  protected Object handleFallback(Throwable throwable) throws Throwable {
-    handleError(throwable);
-    return null;
-  }
+    protected Object handleFallback(Throwable throwable) throws Throwable {
+        handleError(throwable);
+        return null;
+    }
 
-  private void handleError(Throwable throwable) throws Throwable {
-    log.error("Circuit breaker records an error. Detail {}", throwable.getMessage());
-    throw throwable;
-  }
+    private void handleError(Throwable throwable) throws Throwable {
+        log.error("Circuit breaker records an error. Detail {}", throwable.getMessage());
+        throw throwable;
+    }
 }
--- payment/src/main/java/com/yas/payment/service/AbstractCircuitBreakFallbackHandler.java
+++ payment/src/main/java/com/yas/payment/service/AbstractCircuitBreakFallbackHandler.java
@@ -6,16 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 abstract class AbstractCircuitBreakFallbackHandler {
 
     protected void handleBodilessFallback(Throwable throwable) throws Throwable {
-      handleError(throwable);
+        handleError(throwable);
     }
 
     protected Object handleFallback(Throwable throwable) throws Throwable {
-      handleError(throwable);
-      return null;
+        handleError(throwable);
+        return null;
     }
 
     private void handleError(Throwable throwable) throws Throwable {
-      log.error("Circuit breaker records an error. Detail {}", throwable.getMessage());
-      throw throwable;
+        log.error("Circuit breaker records an error. Detail {}", throwable.getMessage());
+        throw throwable;
     }
 }
