91a51f9617048b7e43bafc78f7e85f10821d7782
HoangDoNhat <hoang.donhat@nashtechglobal.com>
2024-08-16 13:12:30 +0700

    Integration test for tax rate repository

---
+++ tax/src/test/java/com/yas/tax/integration/repository/TaxRateRepositoryIT.java
@@ -0,0 +1,73 @@
+package com.yas.tax.integration.repository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.instancio.Select.field;
+
+import com.yas.tax.integration.config.IntegrationTestConfiguration;
+import com.yas.tax.model.TaxClass;
+import com.yas.tax.model.TaxRate;
+import com.yas.tax.repository.TaxClassRepository;
+import com.yas.tax.repository.TaxRateRepository;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@Import(IntegrationTestConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class TaxRateRepositoryIT {
+
+    @Autowired
+    TaxRateRepository taxRateRepository;
+
+    @Autowired
+    TaxClassRepository taxClassRepository;
+
+    TaxClass taxClass;
+
+    TaxRate taxRate;
+
+    @BeforeEach
+    public void setUp(){
+        taxClass = taxClassRepository.save(Instancio.of(TaxClass.class)
+            .set(field(TaxClass::getId), 1L)
+            .create());
+
+        taxRate = taxRateRepository.save(Instancio.of(TaxRate.class)
+                .set(field("taxClass"), taxClass)
+            .create());
+    }
+
+    @AfterEach
+    public void tearDown(){
+        taxRateRepository.deleteAll();
+        taxClassRepository.deleteAll();
+    }
+
+    @Test
+    public void testGetTaxPercent_shouldReturnCorrectTaxRate_whenGivenCorrectParams(){
+        assertThat(taxRateRepository.getTaxPercent(
+            taxRate.getCountryId(),
+            taxRate.getStateOrProvinceId(),
+            taxRate.getZipCode(),
+            taxRate.getTaxClass().getId()))
+            .isEqualTo(taxRate.getRate());
+    }
+
+    @Test
+    public void testGetTaxPercent_shouldReturnNull_whenGivenWrongParams() {
+        assertThat(taxRateRepository.getTaxPercent(
+            Instancio.of(Long.class).create(),
+            Instancio.of(Long.class).create(),
+            Instancio.of(String.class).create(),
+            Instancio.of(Long.class).create()))
+            .isNull();
+    }
+}
