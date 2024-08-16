eb374bb5d02c87d6d39e3d798b5185f714e52f26
Tuan Nguyen Trong Anh <tuan.nguyentronganh@nashtechglobal.com>
2024-08-16 18:03:07 +0700

    Re-format test class

--- promotion/src/test/java/com/yas/promotion/service/PromotionServiceTest.java
+++ promotion/src/test/java/com/yas/promotion/service/PromotionServiceTest.java
@@ -1,5 +1,8 @@
 package com.yas.promotion.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.yas.promotion.PromotionApplication;
 import com.yas.promotion.exception.DuplicatedException;
 import com.yas.promotion.exception.InvalidDateRangeException;
@@ -9,16 +12,12 @@ import com.yas.promotion.utils.Constants;
 import com.yas.promotion.viewmodel.PromotionDetailVm;
 import com.yas.promotion.viewmodel.PromotionListVm;
 import com.yas.promotion.viewmodel.PromotionPostVm;
+import java.time.ZonedDateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.ZonedDateTime;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(classes = PromotionApplication.class)
 class PromotionServiceTest {
@@ -28,7 +27,6 @@ class PromotionServiceTest {
     private PromotionService promotionService;
 
     private Promotion promotion1;
-    private Promotion promotion2;
     private Promotion wrongRangeDatePromotion;
     private PromotionPostVm promotionPostVm;
 
@@ -47,7 +45,7 @@ class PromotionServiceTest {
                 .build();
         promotion1 = promotionRepository.save(promotion1);
 
-        promotion2 = Promotion.builder()
+        Promotion promotion2 = Promotion.builder()
                 .name("Promotion 2")
                 .slug("promotion-2")
                 .description("Description 2")
@@ -58,7 +56,7 @@ class PromotionServiceTest {
                 .startDate(ZonedDateTime.now().minusDays(30))
                 .endDate(ZonedDateTime.now().plusDays(60))
                 .build();
-        promotion2 = promotionRepository.save(promotion2);
+        promotionRepository.save(promotion2);
 
         wrongRangeDatePromotion = Promotion.builder()
             .name("Wrong date")
@@ -101,7 +99,7 @@ class PromotionServiceTest {
 
     @Test
     void createPromotion_WhenExistedSlug_ThenDuplicatedExceptionThrown() {
-        PromotionPostVm promotionPostVm = PromotionPostVm.builder()
+        promotionPostVm = PromotionPostVm.builder()
                 .slug(promotion1.getSlug())
                 .build();
         assertThrows(DuplicatedException.class, () -> promotionService.createPromotion(promotionPostVm),
@@ -110,7 +108,7 @@ class PromotionServiceTest {
 
     @Test
     void createPromotion_WhenEndDateBeforeStartDate_ThenDateRangeExceptionThrown() {
-        PromotionPostVm promotionPostVm = PromotionPostVm.builder()
+        promotionPostVm = PromotionPostVm.builder()
             .endDate(wrongRangeDatePromotion.getEndDate())
             .startDate(wrongRangeDatePromotion.getStartDate())
             .build();
@@ -124,7 +122,7 @@ class PromotionServiceTest {
                 "Promotion", "code",
                 ZonedDateTime.now().minusDays(120), ZonedDateTime.now().plusDays(120));
         assertEquals(2, result.promotionDetailVmList().size());
-        PromotionDetailVm promotionDetailVm = result.promotionDetailVmList().get(0);
+        PromotionDetailVm promotionDetailVm = result.promotionDetailVmList().getFirst();
         assertEquals("promotion-1", promotionDetailVm.slug());
     }
 }
