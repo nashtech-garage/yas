bc8596a1481081225e2f11b4d4fe25a5c057f0f1
bang.vancong <bang.vancong@nashtechglobal.com>
2024-08-23 14:45:34 +0700

    #800 Clean up code

--- sampledata/src/main/java/com/yas/sampledata/utils/SqlScriptExecutor.java
+++ sampledata/src/main/java/com/yas/sampledata/utils/SqlScriptExecutor.java
@@ -3,12 +3,14 @@ package com.yas.sampledata.utils;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SqlScriptExecutor {
 
     public void executeScriptsForSchema(DataSource dataSource, String schema, String locationPattern) {
@@ -20,7 +22,7 @@ public class SqlScriptExecutor {
                 executeSqlScript(dataSource, schema, resource);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
     }
 
@@ -28,9 +30,9 @@ public class SqlScriptExecutor {
         try (Connection connection = dataSource.getConnection()) {
             connection.setSchema(schema); // Set the schema
             ScriptUtils.executeSqlScript(connection, resource);
-            System.out.println("Executed script: " + resource.getFilename() + " on schema: " + schema);
+            log.info("Executed script: " + resource.getFilename() + " on schema: " + schema);
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
     }
 }
