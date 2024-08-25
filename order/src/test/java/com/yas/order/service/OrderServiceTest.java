cc82172ab9e7eb1b66e688081bac7dab28b6c60c
rahul <rahulbudhraj@outlook.com>
2024-08-26 00:39:06 +0530

    adding unit tests to order service

--- order/src/test/java/com/yas/order/service/OrderServiceTest.java
+++ order/src/test/java/com/yas/order/service/OrderServiceTest.java
@@ -1,30 +1,39 @@
 package com.yas.order.service;
 
 import com.yas.order.OrderApplication;
+import com.yas.order.exception.NotFoundException;
 import com.yas.order.model.Order;
 import com.yas.order.model.OrderItem;
+import com.yas.order.model.enumeration.OrderStatus;
 import com.yas.order.repository.OrderItemRepository;
 import com.yas.order.repository.OrderRepository;
+import com.yas.order.utils.AuthenticationUtils;
 import com.yas.order.viewmodel.order.OrderItemPostVm;
 import com.yas.order.viewmodel.order.OrderPostVm;
 import com.yas.order.viewmodel.order.OrderVm;
 import com.yas.order.viewmodel.orderaddress.OrderAddressPostVm;
+import com.yas.order.viewmodel.product.ProductVariationVm;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = OrderApplication.class)
 class OrderServiceTest {
@@ -42,6 +51,7 @@ class OrderServiceTest {
     private OrderItemPostVm orderItemPostVm;
     private OrderAddressPostVm orderAddressPostVm;
     private OrderPostVm orderPostVm;
+    private final String userId = "user1";
 
 
     @BeforeEach
@@ -98,5 +108,55 @@ class OrderServiceTest {
         List<OrderItem> orderItems = orderItemRepository.findAll();
         assertEquals(0, orderItems.size());
     }
+
+    @Test
+    void testGetOrderById_fetch_unsuccessful(){
+        NotFoundException exception = assertThrows(NotFoundException.class, ()-> orderService.getOrderWithItemsById(1));
+        assertEquals("Order 1 is not found", exception.getMessage());
+    }
+
+    @Test
+    void testGetOrderById_fetch_successful(){
+        OrderVm orderVm = orderService.createOrder(orderPostVm);
+        OrderVm order = orderService.getOrderWithItemsById(orderVm.id());
+        assertEquals(1, order.orderItemVms().size());
+    }
+
+    @Test
+    void getIsOrderCompletedWithUserIdAndProductId_unsuccessful(){
+
+        mockSecurity();
+        when(AuthenticationUtils.getCurrentUserId()).thenReturn(userId);
+
+        ProductVariationVm productVariationVm = new ProductVariationVm(1L,"sample_product","abc");
+        when(productService.getProductVariations(1L)).thenReturn(Collections.singletonList(productVariationVm));
+        assertFalse(orderService.isOrderCompletedWithUserIdAndProductId(1L).isPresent());
+    }
+    @Test
+    void getIsOrderCompletedWithUserIdAndProductId_successful(){
+
+        mockSecurity();
+        when(AuthenticationUtils.getCurrentUserId()).thenReturn(userId);
+        OrderVm orderVm = orderService.createOrder(orderPostVm);
+        Optional<Order> order = orderRepository.findById(orderVm.id());
+        if(order.isPresent()){
+            order.get().setOrderStatus(OrderStatus.COMPLETED);
+            this.orderRepository.save(order.get());
+        }
+        ProductVariationVm productVariationVm = new ProductVariationVm(1L,"sample_product","abc");
+        when(productService.getProductVariations(1L)).thenReturn(Collections.singletonList(productVariationVm));
+        assertTrue(orderService.isOrderCompletedWithUserIdAndProductId(1L).isPresent());
+    }
+
+
+    void mockSecurity(){
+        Jwt jwt = mock(Jwt.class);
+        JwtAuthenticationToken authentication = mock(JwtAuthenticationToken.class);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        when(authentication.getToken()).thenReturn(jwt);
+        when(authentication.getName()).thenReturn(userId);
+        when(jwt.getSubject()).thenReturn(userId);
+    }
+
 
 }
