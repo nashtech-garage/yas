ae98bdaed068ef9a6c93f46427eeab25b1f828b0
bang.vancong <bang.vancong@nashtechglobal.com>
2024-08-26 16:08:40 +0700

    #848 Allow execute sampledata without login

--- sampledata/src/main/java/com/yas/sampledata/config/SecurityConfig.java
+++ sampledata/src/main/java/com/yas/sampledata/config/SecurityConfig.java
@@ -8,8 +8,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     @Bean
+    @SuppressWarnings("squid:S4502")
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+            .csrf(csrf -> csrf.disable())  // Updated way to disable CSRF protection
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
             )
