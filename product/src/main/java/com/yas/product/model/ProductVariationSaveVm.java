c0fc89bd4853f85df2a1f70e8b5ef9d1b5164d76
Nguyễn Trí Hải <mochacr0@gmail.com>
2024-08-26 15:06:02 +0700

    #910 Fix checkstyle

--- product/src/main/java/com/yas/product/model/ProductVariationSaveVm.java
+++ product/src/main/java/com/yas/product/model/ProductVariationSaveVm.java
@@ -6,7 +6,10 @@ import java.util.Map;
 
 public interface ProductVariationSaveVm extends ProductProperties {
     Double price();
+
     Long thumbnailMediaId();
+
     List<Long> productImageIds();
+
     Map<Long, String> optionValuesByOptionId();
 }
