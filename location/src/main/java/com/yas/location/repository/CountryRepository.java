c513f6e1734ac4ae16e1ed49ac4e986c8d9e0578
Du Luong Khanh <du.luongkhanh@nashtechglobal.com>
2024-08-26 13:15:26 +0700

    refactor: remove redundant line

--- location/src/main/java/com/yas/location/repository/CountryRepository.java
+++ location/src/main/java/com/yas/location/repository/CountryRepository.java
@@ -2,7 +2,6 @@ package com.yas.location.repository;
 
 import com.yas.location.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
