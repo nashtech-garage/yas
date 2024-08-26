c7f3540dfe40f4853f72e1694df72dba57756457
nashtech-tuannguyenhuu1 <105196741+nashtech-tuannguyenhuu1@users.noreply.github.com>
2024-08-26 20:17:35 +0700

    #916 [Cart] Test for module Cart (#924)
    * #916 [Cart] Test for module Cart
    
    * #916 [Cart] refactor Test for module Cart

---
+++ cart/src/test/java/com/yas/cart/service/ProductServiceTest.java
@@ -0,0 +1,89 @@
+package com.yas.cart.service;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import com.yas.cart.config.ServiceUrlConfig;
+import com.yas.cart.viewmodel.ProductThumbnailVm;
+import java.net.URI;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+class ProductServiceTest {
+
+    RestClient restClient;
+
+    ServiceUrlConfig serviceUrlConfig;
+
+    ProductService productService;
+
+    RestClient.RequestHeadersUriSpec requestHeadersUriSpec;
+
+    RestClient.ResponseSpec responseSpec;
+
+    @BeforeEach
+    void setUp() {
+        restClient = Mockito.mock(RestClient.class);
+        serviceUrlConfig = Mockito.mock(ServiceUrlConfig.class);
+        productService = new ProductService(restClient, serviceUrlConfig);
+        requestHeadersUriSpec = Mockito.mock(RestClient.RequestHeadersUriSpec.class);
+        responseSpec = Mockito.mock(RestClient.ResponseSpec.class);
+    }
+
+    @Test
+    void getProducts_NormalCase_ReturnProductThumbnailVms() {
+
+        List<Long> ids = List.of(1L, 2L, 3L);
+        URI url = UriComponentsBuilder
+            .fromHttpUrl("http://api.yas.local/media")
+            .path("/storefront/products/list-featured")
+            .queryParam("productId", ids)
+            .build()
+            .toUri();
+
+        when(serviceUrlConfig.product()).thenReturn("http://api.yas.local/media");
+        when(restClient.get()).thenReturn(requestHeadersUriSpec);
+        when(requestHeadersUriSpec.uri(url)).thenReturn(requestHeadersUriSpec);
+        when(requestHeadersUriSpec.retrieve()).thenReturn(responseSpec);
+        when(responseSpec.toEntity(new ParameterizedTypeReference<List<ProductThumbnailVm>>() {}))
+            .thenReturn(ResponseEntity.ok(getProductThumbnailVms()));
+
+        List<ProductThumbnailVm> result = productService.getProducts(ids);
+
+        assertThat(result).hasSize(3);
+        assertThat(result.get(0).id()).isEqualTo(1);
+        assertThat(result.get(1).id()).isEqualTo(2);
+        assertThat(result.get(2).id()).isEqualTo(3);
+    }
+
+    private List<ProductThumbnailVm> getProductThumbnailVms() {
+
+        ProductThumbnailVm product1 = new ProductThumbnailVm(
+            1L,
+            "Product 1",
+            "product-1",
+            "http://example.com/product1.jpg"
+        );
+        ProductThumbnailVm product2 = new ProductThumbnailVm(
+            2L,
+            "Product 2",
+            "product-2",
+            "http://example.com/product2.jpg"
+        );
+        ProductThumbnailVm product3 = new ProductThumbnailVm(
+            3L,
+            "Product 3",
+            "product-3",
+            "http://example.com/product3.jpg"
+        );
+
+        return List.of(product1, product2, product3);
+    }
+}
---
+++ cart/src/test/java/com/yas/cart/util/SecurityContextUtils.java
@@ -0,0 +1,23 @@
+package com.yas.cart.util;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityContextUtils {
+
+    private SecurityContextUtils() {
+    }
+
+    public static void setUpSecurityContext(String userName) {
+        Authentication auth = mock(Authentication.class);
+        when(auth.getName()).thenReturn(userName);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(auth);
+        SecurityContextHolder.setContext(securityContext);
+    }
+
+}
