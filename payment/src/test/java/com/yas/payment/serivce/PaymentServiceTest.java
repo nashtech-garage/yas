ab4847f3f84a6848dca178ffe7150f6ee90eb299
Tuan Nguyen Trong Anh <tuan.nguyentronganh@nashtechglobal.com>
2024-08-30 11:54:50 +0700

    Remove unused import

--- payment/src/test/java/com/yas/payment/serivce/PaymentServiceTest.java
+++ payment/src/test/java/com/yas/payment/serivce/PaymentServiceTest.java
@@ -1,6 +1,5 @@
 package com.yas.payment.serivce;
 
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
