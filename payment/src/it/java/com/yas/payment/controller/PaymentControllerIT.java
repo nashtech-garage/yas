ef09712e6fc765cb96de75360c1263d2cac89a56
Tuan Nguyen Trong Anh <tuan.nguyentronganh@nashtechglobal.com>
2024-09-04 23:18:20 +0700

    Add integration test for payment controller

---
+++ payment/src/it/java/com/yas/payment/controller/PaymentControllerIT.java
@@ -0,0 +1,64 @@
+package com.yas.payment.controller;
+
+import com.yas.payment.config.IntegrationTestConfiguration;
+import com.yas.payment.model.Payment;
+import com.yas.payment.repository.PaymentRepository;
+import com.yas.payment.service.OrderService;
+import com.yas.payment.viewmodel.CapturedPayment;
+import com.yas.payment.viewmodel.PaymentOrderStatusVm;
+import io.restassured.RestAssured;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(IntegrationTestConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PaymentControllerIT extends AbstractControllerIT {
+
+    private static final String PAYMENT_CAPTURE_URL = "/v1/storefront/payments/capture";
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    @MockBean
+    OrderService orderService;
+
+    Payment payment;
+    CapturedPayment capturedPayment;
+
+    @BeforeEach
+    void setUp() {
+        payment = paymentRepository.save(Instancio.of(Payment.class).create());
+        capturedPayment = Instancio.of(CapturedPayment.class).create();
+
+        Mockito.when(orderService.updateCheckoutStatus(Mockito.any(CapturedPayment.class)))
+            .thenAnswer(invocation -> Mockito.anyLong());
+
+        Mockito.when(orderService.updateOrderStatus(Mockito.any(PaymentOrderStatusVm.class)))
+            .thenAnswer(invocation -> invocation.getArgument(0));
+    }
+
+    @AfterEach
+    void tearDown() {
+        paymentRepository.deleteAll();
+    }
+
+    @Test
+    void test_capturePayment_shouldReturnOrder() {
+        RestAssured.given(getRequestSpecification())
+            .body(capturedPayment)
+            .post(PAYMENT_CAPTURE_URL)
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .log().ifValidationFails();
+    }
+}
