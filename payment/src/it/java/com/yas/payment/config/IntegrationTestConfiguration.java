a5094f0a7cebeddf10cd966904e32d37a7dbccd0
Tuan Nguyen Trong Anh <tuan.nguyentronganh@nashtechglobal.com>
2024-09-05 15:10:09 +0700

    Add postgres container configuration for it

--- payment/src/it/java/com/yas/payment/config/IntegrationTestConfiguration.java
+++ payment/src/it/java/com/yas/payment/config/IntegrationTestConfiguration.java
@@ -2,6 +2,7 @@ package com.yas.payment.config;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -10,6 +11,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 public class IntegrationTestConfiguration {
 
     @Bean(destroyMethod = "stop")
+    @ServiceConnection
     public PostgreSQLContainer<?> postgresContainer() {
         return new PostgreSQLContainer<>("postgres:16")
             .withReuse(true);
