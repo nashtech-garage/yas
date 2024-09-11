9bec19e75324b2bab898754989c31a24b617045a
LEHOANGGLAM <lam.lecamhoang@unified.com>
2024-09-11 16:22:14 +0700

    update script change id value in payment provider

--- payment/src/main/resources/db/changelog/data/changelog-0001-provider.sql
+++ payment/src/main/resources/db/changelog/data/changelog-0001-provider.sql
@@ -2,6 +2,6 @@
 
 --changeset hoanglam:paypal
 insert into payment_provider (id, is_enabled, name, configure_url, landing_view_component_name, additional_settings)
-    values ('PAYPAL', 'true', 'paypal', 'paypal-config', 'paypal landing view', '{"clientId": "AW7GUe26RhVRlWKHeKHjl43ZqON8NFgJbEOljFDkuBiLlFYWj7mskz77QgVMHkl2M9VBMA5jWMFwxRll", "clientSecret": "ENX1js2V-5bdTe86voCAcyrT6bcRtVzRcvNsdo0XDhDYT_5KEBmlYT7oOBWVVwjPsS4i6bktL1R8eL3j", "mode": "sandbox"}');
+    values ('PaypalPayment', 'true', 'paypal', 'paypal-config', 'paypal landing view', '{"clientId": "AW7GUe26RhVRlWKHeKHjl43ZqON8NFgJbEOljFDkuBiLlFYWj7mskz77QgVMHkl2M9VBMA5jWMFwxRll", "clientSecret": "ENX1js2V-5bdTe86voCAcyrT6bcRtVzRcvNsdo0XDhDYT_5KEBmlYT7oOBWVVwjPsS4i6bktL1R8eL3j", "mode": "sandbox"}');
 
 
--- payment/src/main/resources/db/changelog/data/changelog-0002-provider.sql
+++ payment/src/main/resources/db/changelog/data/changelog-0002-provider.sql
@@ -2,6 +2,6 @@
 
 --changeset hieunc:cod
 insert into payment_provider (id, is_enabled, name, configure_url, landing_view_component_name, additional_settings)
-    values ('COD', 'true', 'Cash on Delivery', 'cod-config', 'cod landing view', '');
+    values ('CodPayment', 'true', 'Cash on Delivery', 'cod-config', 'cod landing view', '');
 
 
