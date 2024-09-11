46b0258c8cfb11afa61c265d82ba599843ad62f7
Khanh Du <110228244+khanhduzz@users.noreply.github.com>
2024-09-11 16:13:28 +0700

    [Product] Fix error when remove images when create, update product, category (#1015)
    * fix remove images when update product
    
    * fix create, update category image
    
    * restore product service with checkstyle

--- product/src/main/java/com/yas/product/service/ProductService.java
+++ product/src/main/java/com/yas/product/service/ProductService.java
@@ -536,18 +536,19 @@ public class ProductService {
     public List<ProductImage> setProductImages(List<Long> imageMediaIds, Product product) {
         List<ProductImage> productImages = new ArrayList<>();
         if (CollectionUtils.isEmpty(imageMediaIds)) {
+            productImageRepository.deleteByProductId(product.getId());
             return productImages;
         }
         if (product.getProductImages() == null) {
             productImages = imageMediaIds.stream()
-                    .map(id -> ProductImage.builder().imageId(id).product(product).build()).toList();
+                .map(id -> ProductImage.builder().imageId(id).product(product).build()).toList();
         } else {
             List<Long> productImageIds = product.getProductImages().stream().map(ProductImage::getImageId).toList();
             List<Long> newImageIds = imageMediaIds.stream().filter(id -> !productImageIds.contains(id)).toList();
             List<Long> deletedImageIds = productImageIds.stream().filter(id -> !imageMediaIds.contains(id)).toList();
             if (CollectionUtils.isNotEmpty(newImageIds)) {
                 productImages = newImageIds.stream()
-                        .map(id -> ProductImage.builder().imageId(id).product(product).build()).toList();
+                    .map(id -> ProductImage.builder().imageId(id).product(product).build()).toList();
             }
             if (CollectionUtils.isNotEmpty(deletedImageIds)) {
                 productImageRepository.deleteByImageIdInAndProductId(deletedImageIds, product.getId());
