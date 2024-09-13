d705e6ed1a8ecfb18778809af0703497d23865fa
Tuan Nguyen Trong Anh <tuan.nguyentronganh@nashtechglobal.com>
2024-09-13 09:48:41 +0700

    Reformat storefront service to use apiClientService

--- storefront/modules/address/services/AddressService.tsx
+++ storefront/modules/address/services/AddressService.tsx
@@ -1,31 +1,24 @@
 import { Address } from '../models/AddressModel';
+import apiClientService from '@/common/services/ApiClientService';
+
+const baseUrl = '/api/location/storefront/addresses';
 
 export async function createAddress(address: Address) {
-  const response = await fetch(`/api/location/storefront/addresses`, {
-    method: 'POST',
-    headers: { 'Content-type': 'application/json' },
-    body: JSON.stringify(address),
-  });
+  const response = await apiClientService.post(baseUrl, JSON.stringify(address));
   return response.json();
 }
 
 export async function updateAddress(id: string, address: Address) {
-  const response = await fetch(`/api/location/storefront/addresses/${id}`, {
-    method: 'PUT',
-    headers: { 'Content-type': 'application/json' },
-    body: JSON.stringify(address),
-  });
+  const response = await apiClientService.put(`${baseUrl}/${id}`, JSON.stringify(address));
   return response;
 }
 
 export async function getAddress(id: string) {
-  const response = await fetch(`/api/location/storefront/addresses/${id}`);
+  const response = await apiClientService.get(`${baseUrl}/${id}`);
   return await response.json();
 }
 
 export async function deleteAddress(id: number) {
-  const response = await fetch(`/api/location/storefront/addresses/${id}`, {
-    method: 'DELETE',
-  });
+  const response = await apiClientService.delete(`${baseUrl}/${id}`);
   return response;
 }
