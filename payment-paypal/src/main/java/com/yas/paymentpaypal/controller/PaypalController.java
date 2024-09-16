2880afe0ccc598f5c9a07f0bef2d67b50296bebc
HARVEYNASH\hieu.nguyenchi5 <hieu.nguyenchi5@nashtechglobal.com>
2024-09-16 10:47:55 +0700

    add path payment-paypal

--- payment-paypal/src/main/java/com/yas/paymentpaypal/controller/PaypalController.java
+++ payment-paypal/src/main/java/com/yas/paymentpaypal/controller/PaypalController.java
@@ -10,11 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/payment-paypal")
 public class PaypalController {
     private final PaypalService paypalService;
 
