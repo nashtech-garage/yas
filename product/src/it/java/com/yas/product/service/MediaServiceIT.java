bbe8c4a1cacdbba77cd87dcdf86ab8116f7163ec
Du Luong Khanh <du.luongkhanh@nashtechglobal.com>
2024-09-17 10:43:12 +0700

    add test category and brand

--- product/src/it/java/com/yas/product/service/MediaServiceIT.java
+++ product/src/it/java/com/yas/product/service/MediaServiceIT.java
@@ -3,27 +3,21 @@ package com.yas.product.service;
 import static com.yas.product.constant.TestConstants.CIRCUIT_BREAKER_NAME;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.yas.product.config.IntegrationTestConfiguration;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-@Testcontainers
+@Import(IntegrationTestConfiguration.class)
 class MediaServiceIT {
     @Autowired
     private MediaService mediaService;
     @Autowired
     private CircuitBreakerRegistry circuitBreakerRegistry;
-
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:16");
 
     @Test
     void test_getMedia_shouldThrowCallNotPermittedException_whenCircuitBreakerIsOpen() {
