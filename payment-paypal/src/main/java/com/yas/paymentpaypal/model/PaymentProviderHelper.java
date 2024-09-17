06f5ee2f96e5272a5e3c4bb2da94039e916db1c9
atuan21 <79851373+HnKnA@users.noreply.github.com>
2024-09-17 09:39:44 +0700

    Configure paypal id on paymentProviderHelper (#1036)
    Co-authored-by: Tuan Nguyen Trong Anh <tuan.nguyentronganh@nashtechglobal.com>

--- payment-paypal/src/main/java/com/yas/paymentpaypal/model/PaymentProviderHelper.java
+++ payment-paypal/src/main/java/com/yas/paymentpaypal/model/PaymentProviderHelper.java
@@ -1,7 +1,7 @@
 package com.yas.paymentpaypal.model;
 
 public class PaymentProviderHelper {
-    public static final String PAYPAL_PAYMENT_PROVIDER_ID = "PaypalPayment";
+    public static final String PAYPAL_PAYMENT_PROVIDER_ID = "PAYPAL";
 
     private PaymentProviderHelper() {
         // Private constructor to prevent instantiation
