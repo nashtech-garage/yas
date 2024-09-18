ec3a22656ad95618bdb9c0f704bb4486dac95285
atuan21 <79851373+HnKnA@users.noreply.github.com>
2024-09-18 09:46:34 +0700

    #993 [Storefront] Reformat order flow to use api client service (#1040)
    * Reformat storefront service to use apiClientService
    
    
    ---------
    
    Co-authored-by: Tuan Nguyen Trong Anh <tuan.nguyentronganh@nashtechglobal.com>

--- storefront/modules/paymentPaypal/services/PaymentPaypalService.ts
+++ storefront/modules/paymentPaypal/services/PaymentPaypalService.ts
@@ -1,25 +1,24 @@
 import { InitPaymentPaypalRequest } from '@/modules/paymentPaypal/models/InitPaymentPaypalRequest';
 import { InitPaymentPaypalResponse } from '@/modules/paymentPaypal/models/InitPaymentPaypalResponse';
 import { CapturePaymentPaypalResponse } from '@/modules/paymentPaypal/models/CapturePaymentPaypalResponse';
+import apiClientService from '@/common/services/ApiClientService';
+
+const baseUrl = '/api/payment-paypal';
 
 export async function initPaymentPaypal(
   paymentPaypalRequest: InitPaymentPaypalRequest
 ): Promise<InitPaymentPaypalResponse> {
-  const res = await fetch('/api/payment-paypal/init', {
-    method: 'POST',
-    body: JSON.stringify(paymentPaypalRequest),
-    headers: { 'Content-type': 'application/json; charset=UTF-8' },
-  });
+  const res = await apiClientService.post(`${baseUrl}/init`, JSON.stringify(paymentPaypalRequest));
   if (res.ok) {
     return res.json();
   }
-  return Promise.reject(res.status);
+  throw new Error(res.statusText);
 }
 
 export async function capturePaymentPaypal(token?: string): Promise<CapturePaymentPaypalResponse> {
-  const res = await fetch(`/api/payment-paypal/capture?token=${token}`);
+  const res = await apiClientService.get(`${baseUrl}/capture?token=${token}`);
   if (res.ok) {
     return res.json();
   }
-  return Promise.reject(res.status);
+  throw new Error(res.statusText);
 }
