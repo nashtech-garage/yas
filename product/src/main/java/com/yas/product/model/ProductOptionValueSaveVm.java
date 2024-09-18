098f3ede06f38f25a62c09c41528fb9706914a2f
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-09-18 16:50:41 +0700

    Combine 2 Vm

--- product/src/main/java/com/yas/product/model/ProductOptionValueSaveVm.java
+++ product/src/main/java/com/yas/product/model/ProductOptionValueSaveVm.java
@@ -1,5 +1,14 @@
 package com.yas.product.model;
 
-public interface ProductOptionValueSaveVm {
-    Long productOptionId();
+//public interface ProductOptionValueSaveVm {
+import java.util.List;
+
+//    Long productOptionId();
+//}
+public record ProductOptionValueSaveVm(
+        Long productOptionId,
+        String displayType,
+        Integer displayOrder,
+        List<String> value) {
+
 }
--- product/src/main/java/com/yas/product/viewmodel/productoption/ProductOptionValuePostVm.java
+++ product/src/main/java/com/yas/product/viewmodel/productoption/ProductOptionValuePostVm.java
@@ -1,11 +1,11 @@
-package com.yas.product.viewmodel.productoption;
-
-import com.yas.product.model.ProductOptionValueSaveVm;
-import java.util.List;
-
-public record ProductOptionValuePostVm(
-        Long productOptionId,
-        String displayType,
-        Integer displayOrder,
-        List<String> value) implements ProductOptionValueSaveVm {
-}
+//package com.yas.product.viewmodel.productoption;
+//
+//import com.yas.product.model.ProductOptionValueSaveVm;
+//import java.util.List;
+//
+//public record ProductOptionValuePostVm(
+//        Long productOptionId,
+//        String displayType,
+//        Integer displayOrder,
+//        List<String> value) implements ProductOptionValueSaveVm {
+//}
