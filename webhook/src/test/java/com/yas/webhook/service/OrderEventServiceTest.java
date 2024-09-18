00cb0267640be6b1a9c18d93281e7efe87485bb1
HoangDoNhat <hoang.donhat@nashtechglobal.com>
2024-09-18 10:05:16 +0700

    Add test for webhook to reach 80% coverage

--- webhook/src/test/java/com/yas/webhook/service/OrderEventServiceTest.java
+++ webhook/src/test/java/com/yas/webhook/service/OrderEventServiceTest.java
@@ -1,6 +1,7 @@
 package com.yas.webhook.service;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -61,5 +62,67 @@ class OrderEventServiceTest {
         orderEventService.onOrderEvent(objectNode);
 
         verify(webhookEventNotificationRepository).save(any(WebhookEventNotification.class));
+    }
+
+    @Test
+    void test_onOrderEvent_shouldNotExceptionForUpdateOrderStatus() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("op", "u");
+        ObjectNode before = objectMapper.createObjectNode();
+        before.put("order_status","NEW");
+        objectNode.set("before", before);
+        ObjectNode after = objectMapper.createObjectNode();
+        after.put("order_status","PAID");
+        objectNode.set("after", after);
+
+        Event event = new Event();
+        WebhookEvent webhookEvent = new WebhookEvent();
+        Webhook webhook = new Webhook();
+        List<WebhookEvent> webhookEvents = List.of(webhookEvent);
+
+        event.setWebhookEvents(webhookEvents);
+        webhookEvent.setWebhook(webhook);
+
+        WebhookEventNotification notification = new WebhookEventNotification();
+        notification.setWebhookEventId(1L);
+
+        when(eventRepository.findByName(EventName.ON_ORDER_STATUS_UPDATED)).thenReturn(Optional.of(event));
+        when(webhookEventNotificationRepository.save(any(WebhookEventNotification.class))).thenReturn(notification);
+
+        orderEventService.onOrderEvent(objectNode);
+
+        verify(webhookEventNotificationRepository).save(any(WebhookEventNotification.class));
+    }
+
+    @Test
+    void test_onOrderEvent_shouldNotNotifyWhenOpIsUnknown() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("op", "k");
+
+        orderEventService.onOrderEvent(objectNode);
+
+        verify(webhookEventNotificationRepository, times(0)).save(any(WebhookEventNotification.class));
+    }
+
+    @Test
+    void test_onOrderEvent_shouldNotNotifyWhenOpIsUpdateButOrderStatusNotChange() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("op", "u");
+        ObjectNode before = objectMapper.createObjectNode();
+        before.put("order_status","NEW");
+        objectNode.set("before", before);
+        ObjectNode after = objectMapper.createObjectNode();
+        after.put("order_status","NEW");
+        objectNode.set("after", after);
+
+        orderEventService.onOrderEvent(objectNode);
+
+        verify(webhookEventNotificationRepository, times(0)).save(any(WebhookEventNotification.class));
     }
 }
