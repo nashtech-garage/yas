c6bea0615325498231ef44455fc1bcca3aeb517a
nashtech-huyphamphu <huy.phamphuvu@nashtechglobal.com>
2024-09-19 12:17:10 +0700

    #882 Create a shared project

--- cart/src/it/java/com/yas/cart/service/ProductServiceIT.java
+++ cart/src/it/java/com/yas/cart/service/ProductServiceIT.java
@@ -13,12 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.ComponentScan;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
+@ComponentScan(basePackages = {"com.yas.commonlibrary"})
 class ProductServiceIT {
     @Container
     @ServiceConnection
