a9eeb053a4b6d5c97c4f56d331a3046e86f10dad
minh.tranquang <minh.tranquang@nashtechglobal.com>
2024-09-26 07:52:58 +0700

    [Search] Add integration test

--- search/src/it/java/com/yas/search/config/ElasticTestContainer.java
+++ search/src/it/java/com/yas/search/config/ElasticTestContainer.java
@@ -6,7 +6,7 @@ public class ElasticTestContainer extends ElasticsearchContainer {
 
   private static final String DOCKER_ELASTIC = "docker.elastic.co/elasticsearch/elasticsearch:7.17.6";
 
-  private static final String CLUSTER_NAME = "sample-cluster";
+  private static final String CLUSTER_NAME = "cluster.name";
 
   private static final String ELASTIC_SEARCH = "elasticsearch";
 
