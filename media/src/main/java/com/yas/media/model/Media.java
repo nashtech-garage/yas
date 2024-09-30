0a795ef784a9e33f6ecb080deb0f99892015c600
nashtech-huyphamphu <huy.phamphuvu@nashtechglobal.com>
2024-09-30 10:54:03 +0700

    #882 Create a shared project

--- media/src/main/java/com/yas/media/model/Media.java
+++ media/src/main/java/com/yas/media/model/Media.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
