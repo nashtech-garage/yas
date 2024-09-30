8cbfc393a6f6367fb30deb6cc5b038628aecdaff
Tuan Nguyen Trong Anh <tuan.nguyentronganh@nashtechglobal.com>
2024-09-30 14:00:46 +0700

    Remove unnecessary methods on search module

--- search/src/it/java/com/yas/search/config/IntegrationTestConfiguration.java
+++ search/src/it/java/com/yas/search/config/IntegrationTestConfiguration.java
@@ -1,25 +1,13 @@
 package com.yas.search.config;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertySource;
 import org.springframework.test.context.DynamicPropertyRegistry;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.elasticsearch.ElasticsearchContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
-public class IntegrationTestConfiguration extends ElasticsearchContainer implements
-    ApplicationContextInitializer<ConfigurableApplicationContext> {
+public class IntegrationTestConfiguration {
 
     @Bean(destroyMethod = "stop")
     public KeycloakContainer keycloakContainer(DynamicPropertyRegistry registry) {
@@ -37,27 +25,6 @@ public class IntegrationTestConfiguration extends ElasticsearchContainer impleme
     @Bean(destroyMethod = "stop")
     @ServiceConnection
     public ElasticTestContainer elasticTestContainer() {
-        ElasticTestContainer elasticTestContainer = new ElasticTestContainer();
-        elasticTestContainer.start();
-        return elasticTestContainer;
-    }
-
-    @Override
-    public void initialize(ConfigurableApplicationContext applicationContext) {
-        ConfigurableEnvironment environment = applicationContext.getEnvironment();
-        KafkaContainer kafka = new KafkaContainer(
-            DockerImageName.parse("confluentinc/cp-kafka:7.2.2.arm64"));
-        kafka.start();
-        setProperties(environment, "spring.kafka.bootstrap-servers", kafka.getBootstrapServers());
-    }
-
-    private void setProperties(ConfigurableEnvironment environment, String name, Object value) {
-        MutablePropertySources sources = environment.getPropertySources();
-        PropertySource<?> source = sources.get(name);
-        if (source == null) {
-            source = new MapPropertySource(name, new HashMap<>());
-            sources.addFirst(source);
-        }
-        ((Map<String, Object>) source.getSource()).put(name, value);
+        return new ElasticTestContainer();
     }
 }
