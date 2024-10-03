d075d6c5bd4c6ae1486248fbcb5a98f4107baf1a
Phuoc Nguyen <phuoc.nguyenba@nashtechglobal.com>
2024-10-03 10:24:39 +0700

    #1102 - Create recommendation project, product vector ddl
    -config class to define beans

---
+++ recommendation/src/main/java/com/yas/recommendation/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.yas.recommendation.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    TokenTextSplitter tokenTextSplitter() {
+        return new TokenTextSplitter();
+    }
+
+    @Bean
+    ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+}
