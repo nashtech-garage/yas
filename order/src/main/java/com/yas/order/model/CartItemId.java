475d31f2151f529cefd24d6c26292b599264aaa3
Nguyễn Trí Hải <mochacr0@gmail.com>
2024-10-04 10:22:36 +0700

    fix sonar

--- order/src/main/java/com/yas/order/model/CartItemId.java
+++ order/src/main/java/com/yas/order/model/CartItemId.java
@@ -2,33 +2,17 @@ package com.yas.order.model;
 
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
+@Data
 public class CartItemId implements Serializable {
     private String customerId;
     private Long productId;
 
     public static CartItemId of(String customerId, Long productId) {
         return new CartItemId(customerId, productId);
-    }
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
     }
 }
