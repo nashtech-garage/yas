7e92d365abb7c1b2217ff522002799a1a12711db
Nguyễn Trí Hải <mochacr0@gmail.com>
2024-10-04 10:44:35 +0700

    fix sonar

--- order/src/main/java/com/yas/order/model/CartItem.java
+++ order/src/main/java/com/yas/order/model/CartItem.java
@@ -6,17 +6,15 @@ import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "cart_item")
 @IdClass(CartItemId.class)
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
+@lombok.Getter
+@lombok.Setter
 @Builder
 public class CartItem extends AbstractAuditEntity {
     @Id
