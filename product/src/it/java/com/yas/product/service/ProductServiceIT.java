a500f68ca4267773a84d44e6e0b8caefe86cefa2
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-10-04 10:05:57 +0700

    Reformat

--- product/src/it/java/com/yas/product/service/ProductServiceIT.java
+++ product/src/it/java/com/yas/product/service/ProductServiceIT.java
@@ -6,9 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.yas.commonlibrary.IntegrationTestConfiguration;
@@ -34,7 +31,6 @@ import com.yas.product.viewmodel.product.ProductListVm;
 import com.yas.product.viewmodel.product.ProductThumbnailGetVm;
 import com.yas.product.viewmodel.product.ProductThumbnailVm;
 import com.yas.product.viewmodel.product.ProductsGetVm;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -54,7 +50,7 @@ import org.springframework.context.annotation.Import;
 @Import(IntegrationTestConfiguration.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ProductServiceIT {
-    private final ZonedDateTime CREATED_ON = ZonedDateTime.now();
+
     @Autowired
     private ProductRepository productRepository;
     @Autowired
@@ -76,19 +72,21 @@ class ProductServiceIT {
     private List<Product> products;
     private List<Category> categoryList;
     private List<ProductCategory> productCategoryList;
+    private List<ProductOption> productOptions;
     private Category category1;
     private Category category2;
     private Brand brand1;
     private Brand brand2;
-    private int pageNo = 0;
-    private int pageSize = 5;
-    private int totalPage = 2;
+    private final int pageNo = 0;
+    private final int pageSize = 5;
+    private final int totalPage = 2;
     private NoFileMediaVm noFileMediaVm;
 
     @BeforeEach
     void setUp() {
         noFileMediaVm = new NoFileMediaVm(1L, "caption", "fileName", "mediaType", "url");
         when(mediaService.getMedia(1L)).thenReturn(noFileMediaVm);
+        generateTestData();
     }
 
     private void generateTestData() {
@@ -132,16 +130,15 @@ class ProductServiceIT {
                 product.setBrand(brand1);
                 product.setPrice(5.0);
             }
-            product.setCreatedOn(CREATED_ON);
             products.add(product);
         }
-        List<Product> productsDb = productRepository.saveAll(products);
+        productRepository.saveAll(products);
 
         productCategoryList = new ArrayList<>();
-        for (int i = 1; i <= productsDb.size(); i++) {
-            Product productDb = productsDb.get(i - 1);
+        for (int i = 1; i <= products.size(); i++) {
+            Product product = products.get(i - 1);
             ProductCategory productCategory = new ProductCategory();
-            productCategory.setProduct(productDb);
+            productCategory.setProduct(product);
             if (i % 2 == 0) {
                 productCategory.setCategory(category2);
             } else {
@@ -150,21 +147,34 @@ class ProductServiceIT {
             productCategoryList.add(productCategory);
         }
         productCategoryRepository.saveAll(productCategoryList);
+
+        initProductOptions();
+    }
+
+    private void initProductOptions() {
+        ProductOption productOption1 = new ProductOption();
+        productOption1.setName("productOption1");
+        ProductOption productOption2 = new ProductOption();
+        productOption2.setName("productOption2");
+        ProductOption productOption3 = new ProductOption();
+        productOption3.setName("productOption3");
+        productOptions = List.of(productOption1, productOption2, productOption3);
+        productOptionRepository.saveAll(productOptions);
     }
 
     private Product getVariant(Product parent) {
         return Product.builder()
-            .name("product variant name")
-            .slug("product variant slug")
-            .isAllowedToOrder(true)
-            .isPublished(true)
-            .isFeatured(true)
-            .isVisibleIndividually(true)
-            .stockTrackingEnabled(true)
-            .thumbnailMediaId(1L)
-            .taxClassId(1L)
-            .parent(parent)
-            .build();
+                .name("product variant name")
+                .slug("product variant slug")
+                .isAllowedToOrder(true)
+                .isPublished(true)
+                .isFeatured(true)
+                .isVisibleIndividually(true)
+                .stockTrackingEnabled(true)
+                .thumbnailMediaId(1L)
+                .taxClassId(1L)
+                .parent(parent)
+                .build();
     }
 
     @AfterEach
@@ -181,12 +191,11 @@ class ProductServiceIT {
     @DisplayName("Get product feature success then return list ProductThumbnailVm")
     @Test
     void getFeaturedProducts_WhenEverythingIsOkay_Success() {
-        generateTestData();
         ProductFeatureGetVm actualResponse = productService.getListFeaturedProducts(pageNo, pageSize);
         assertThat(actualResponse.totalPage()).isEqualTo(totalPage);
         assertThat(actualResponse.productList()).hasSize(5);
         Map<String, Product> productMap
-            = products.stream().collect(Collectors.toMap(Product::getSlug, product -> product));
+                = products.stream().collect(Collectors.toMap(Product::getSlug, product -> product));
         for (int i = 0; i < actualResponse.productList().size(); i++) {
             ProductThumbnailGetVm productThumbnailGetVm = actualResponse.productList().get(i);
             Product product = productMap.get(productThumbnailGetVm.slug());
@@ -194,11 +203,9 @@ class ProductServiceIT {
         }
     }
 
-
     @DisplayName("Get products by brand when brand is available with slug then success")
     @Test
     void getProductsByBrand_BrandSlugIsValid_Success() {
-        generateTestData();
         List<ProductThumbnailVm> actualResponse = productService.getProductsByBrand(brand1.getSlug());
         assertEquals(5, actualResponse.size());
     }
@@ -207,8 +214,8 @@ class ProductServiceIT {
     @Test
     void getProductsByBrand_BrandIsNonExist_ThrowsNotFoundException() {
         NotFoundException exception = assertThrows(NotFoundException.class,
-            () -> productService.getProductsByBrand("brandSlug1"));
-        assertEquals(String.format("Brand %s is not found", "brandSlug1"), exception.getMessage());
+                () -> productService.getProductsByBrand("nonExistBrandSlug1"));
+        assertEquals(String.format("Brand %s is not found", "nonExistBrandSlug1"), exception.getMessage());
     }
 
     @Test
@@ -220,14 +227,12 @@ class ProductServiceIT {
 
     @Test
     void getProduct_whenProductIdValid_shouldSuccess() {
-        generateTestData();
         List<Product> productDbList = productRepository.findAll();
         assertNotNull(productService.getProductById(productDbList.getFirst().getId()));
     }
 
     @Test
     void getListFeaturedProductsByListProductIds_whenAllProductIdsValid_shouldSuccess() {
-        generateTestData();
         List<Product> productDbList = productRepository.findAll();
         List<Long> ids = productDbList.stream().map(Product::getId).collect(Collectors.toList());
         assertEquals(10, productService.getFeaturedProductsById(ids).size());
@@ -235,78 +240,64 @@ class ProductServiceIT {
 
     @Test
     void getProductsWithFilter_WhenFilterByBrandNameAndProductName_ThenSuccess() {
-        generateTestData();
         ProductListGetVm actualResponse
-            = productService.getProductsWithFilter(pageNo, pageSize, "product1", brand1.getName());
+                = productService.getProductsWithFilter(pageNo, pageSize, "product1", brand1.getName());
         assertEquals(1, actualResponse.productContent().size());
     }
 
     @Test
     void getProductsWithFilter_WhenFilterByBrandName_ThenSuccess() {
-        generateTestData();
         ProductListGetVm actualResponse = productService.getProductsWithFilter(pageNo, pageSize, "", brand1.getName());
         assertEquals(5, actualResponse.productContent().size());
     }
 
     @Test
     void getProductsWithFilter_WhenFilterByProductName_ThenSuccess() {
-        generateTestData();
         ProductListGetVm actualResponse = productService.getProductsWithFilter(pageNo, pageSize, "product9", "");
         assertEquals(1, actualResponse.productContent().size());
     }
 
     @Test
     void getProductsWithFilter_whenFindAll_thenSuccess() {
-        generateTestData();
         ProductListGetVm actualResponse
-            = productService.getProductsWithFilter(pageNo, pageSize, "product", brand2.getName());
+                = productService.getProductsWithFilter(pageNo, pageSize, "product", brand2.getName());
         assertEquals(5, actualResponse.productContent().size());
-
     }
 
     @Test
     void getProductsFromCategory_WhenFindAllByCategory_ThenSuccess() {
-        generateTestData();
         ProductListGetFromCategoryVm actualResponse
-            = productService.getProductsFromCategory(pageNo, pageSize, "categorySlug1");
+                = productService.getProductsFromCategory(pageNo, pageSize, "categorySlug1");
         assertEquals(5, actualResponse.productContent().size());
     }
 
     @Test
     void getProductsFromCategory_CategoryIsNonExist_ThrowsNotFoundException() {
-        generateTestData();
         String categorySlug = "laptop-macbook";
         NotFoundException exception = assertThrows(NotFoundException.class,
-            () -> productService.getProductsFromCategory(pageNo, pageSize, categorySlug));
+                () -> productService.getProductsFromCategory(pageNo, pageSize, categorySlug));
         assertThat(exception.getMessage()).isEqualTo(String.format("Category %s is not found", categorySlug));
     }
 
     @Test
     void deleteProduct_givenParentProduct_thenSuccess() {
-        generateTestData();
         Long id = productRepository.findAll().getFirst().getId();
         productService.deleteProduct(id);
         Optional<Product> result = productRepository.findById(id);
         // Soft delete, set published to false
         assertTrue(result.isPresent());
         assertFalse(result.get().isPublished());
-
     }
 
     @Test
     void deleteProduct_givenVariant_thenSuccess() {
-
-        generateTestData();
-
         String value = "red";
         ProductOption productOption = new ProductOption();
         productOption.setName(value);
-        productOption.setCreatedOn(CREATED_ON);
 
         String cpu = "I5";
         ProductOption productOption2 = new ProductOption();
         productOption2.setName(cpu);
-        productOption2.setCreatedOn(CREATED_ON);
 
         ProductOptionCombination productOptionCombination = new ProductOptionCombination();
         productOptionCombination.setValue(value);
@@ -329,9 +320,9 @@ class ProductServiceIT {
         productOptionCombination2.setProductOption(afterProductOption2);
 
         ProductOptionCombination productOptionCombinationSaved
-            = productOptionCombinationRepository.save(productOptionCombination);
+                = productOptionCombinationRepository.save(productOptionCombination);
         ProductOptionCombination productOptionCombinationSaved2
-            = productOptionCombinationRepository.save(productOptionCombination2);
+                = productOptionCombinationRepository.save(productOptionCombination2);
 
         assertTrue(productOptionCombinationRepository.findById(productOptionCombinationSaved.getId()).isPresent());
         assertTrue(productOptionCombinationRepository.findById(productOptionCombinationSaved2.getId()).isPresent());
@@ -343,13 +334,10 @@ class ProductServiceIT {
         assertFalse(result.get().isPublished());
         assertFalse(productOptionCombinationRepository.findById(productOptionCombinationSaved.getId()).isPresent());
         assertFalse(productOptionCombinationRepository.findById(productOptionCombinationSaved2.getId()).isPresent());
-
     }
 
     @Test
     void deleteProduct_givenNoOption_noDeleteAll() {
-
-        generateTestData();
         Product parent = productRepository.findAll().getFirst();
         Product product = getVariant(parent);
         Product afterProduct = productRepository.save(product);
@@ -359,7 +347,6 @@ class ProductServiceIT {
 
         assertTrue(result.isPresent());
         assertFalse(result.get().isPublished());
-
     }
 
     @Test
@@ -370,12 +357,11 @@ class ProductServiceIT {
 
     @Test
     void getProductsByMultiQuery_WhenFilterByBrandNameAndProductName_ThenSuccess() {
-        generateTestData();
         Double startPrice = 1.0;
         Double endPrice = 10.0;
         String productName = "product2";
         ProductsGetVm result
-            = productService.getProductsByMultiQuery(pageNo, pageSize, productName, category2.getSlug(), startPrice, endPrice);
+                = productService.getProductsByMultiQuery(pageNo, pageSize, productName, category2.getSlug(), startPrice, endPrice);
 
         // Assert result
         assertEquals(1, result.productContent().size());
@@ -392,17 +378,15 @@ class ProductServiceIT {
 
     @Test
     void getProductsByCategoryIds_WhenFindAllByCategoryIds_ThenSuccess() {
-        generateTestData();
         List<Long> categoryIds = List.of(1L, 2L);
-        List<ProductListVm>  actualResponse = productService.getProductByCategoryIds(categoryIds);
+        List<ProductListVm> actualResponse = productService.getProductByCategoryIds(categoryIds);
         assertEquals(0, actualResponse.size());
     }
 
     @Test
     void getProductsByBrandIds_WhenFindAllByBrandIds_ThenSuccess() {
-        generateTestData();
         List<Long> brandIds = List.of(brand1.getId(), brand2.getId());
-        List<ProductListVm>  actualResponse = productService.getProductByBrandIds(brandIds);
+        List<ProductListVm> actualResponse = productService.getProductByBrandIds(brandIds);
         assertEquals(10, actualResponse.size());
         assertEquals("product1", actualResponse.getFirst().name());
         assertEquals("slug1", actualResponse.getFirst().slug());
