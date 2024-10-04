e15e7819fcbd8f9db8dd4de1f8b9fac2a669c82e
nashtech-huyphamphu <huy.phamphuvu@nashtechglobal.com>
2024-10-04 11:30:31 +0700

    #882 Create a shared project: Apply common ApiExceptionHandler for all modules

--- common-library/src/main/java/com/yas/commonlibrary/config/CorsConfig.java
+++ common-library/src/main/java/com/yas/commonlibrary/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedMethods("*")
-                    .allowedOrigins(corsProperties.getAllowedOrigins()).allowedHeaders("*");
+                    .allowedOrigins("*").allowedHeaders("*");
             }
         };
     }
