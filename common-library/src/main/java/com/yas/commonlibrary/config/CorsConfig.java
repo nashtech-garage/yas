37d93a71aca3cb39bc086fe1dcf2c719289340a4
nashtech-huyphamphu <huy.phamphuvu@nashtechglobal.com>
2024-10-04 10:47:58 +0700

    #882 Create a shared project: Apply common ApiExceptionHandler for all modules

--- common-library/src/main/java/com/yas/commonlibrary/config/CorsConfig.java
+++ common-library/src/main/java/com/yas/commonlibrary/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.yas.commonlibrary.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,13 +8,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+    private final CorsProperties corsProperties;
+
+    @Autowired
+    public CorsConfig(CorsProperties corsProperties) {
+        this.corsProperties = corsProperties;
+    }
+
     @Bean
     public WebMvcConfigurer corsConfigure() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*")
-                    .allowedHeaders("*");
+                registry.addMapping("/**").allowedMethods("*")
+                    .allowedOrigins(corsProperties.getAllowedOrigins()).allowedHeaders("*");
             }
         };
     }
---
+++ common-library/src/main/java/com/yas/commonlibrary/config/CorsProperties.java
@@ -0,0 +1,19 @@
+package com.yas.commonlibrary.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+    private String[] allowedOrigins;
+
+    // Getter và Setter cho allowedOrigins
+    public String[] getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(String[] allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
