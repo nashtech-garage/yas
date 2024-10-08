e8a874937609a615531c69c5914594a0e3000e8f
Nguyễn Trí Hải <mochacr0@gmail.com>
2024-10-09 00:48:39 +0700

    refactor & update tests

--- cart/src/main/java/com/yas/cart/viewmodel/CartItemPutVmV2.java
+++
@@ -1,8 +0,0 @@
-package com.yas.cart.viewmodel;
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-
-@Builder
-public record CartItemPutVmV2(@NotNull @Min(1) Integer quantity) {}
