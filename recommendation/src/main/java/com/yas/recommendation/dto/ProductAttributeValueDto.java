d1d33fd565a1b8b198f4913ac9851d132a9b48d4
Duy Le Van <duy.levan@nashtechglobal.com>
2024-10-08 16:49:13 +0700

    #1102 - Fix checkstyle

--- recommendation/src/main/java/com/yas/recommendation/dto/ProductAttributeValueDto.java
+++ recommendation/src/main/java/com/yas/recommendation/dto/ProductAttributeValueDto.java
@@ -4,17 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import java.util.Map;
-
 @JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ProductVariationDTO(
-        Long id,
-        String name,
-        String slug,
-        String sku,
-        String gtin,
-        Double price,
-        Map<Long, String> options
+public record ProductAttributeValueDto(
+        long id,
+        String nameProductAttribute,
+        String value
 ) {
 }
