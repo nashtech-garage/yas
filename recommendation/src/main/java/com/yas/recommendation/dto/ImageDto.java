af7b29f5fad5c66dd6920b5878d26ee48cac6b03
Chinh Duong <chinh.duongthi@harveynash.vn>
2024-10-08 23:27:18 +0700

    #1116-[Recommendation Service]-Create service to get product detail for storing pgVector - Remane DTO

--- recommendation/src/main/java/com/yas/recommendation/dto/ImageDto.java
+++
@@ -1,6 +0,0 @@
-package com.yas.recommendation.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record ImageDto(Long id, String url) {}
