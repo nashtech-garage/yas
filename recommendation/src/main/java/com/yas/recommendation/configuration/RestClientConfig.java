e89924254fdb456de10097ccdb19b9310924d197
Duy Le Van <duy.levan@nashtechglobal.com>
2024-10-08 17:03:32 +0700

    #1102 - Clean up

--- recommendation/src/main/java/com/yas/recommendation/configuration/RestClientConfig.java
+++ recommendation/src/main/java/com/yas/recommendation/configuration/RestClientConfig.java
@@ -1,6 +1,5 @@
 package com.yas.recommendation.configuration;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
