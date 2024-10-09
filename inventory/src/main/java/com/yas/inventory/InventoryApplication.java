a3f6d42ff5cd8cdc4c4e0bcba3687b18d01f0a1e
nashtech-huyphamphu <huy.phamphuvu@nashtechglobal.com>
2024-10-09 23:13:10 +0700

    #882 Create a shared project: Apply common ApiExceptionHandler for all modules

--- inventory/src/main/java/com/yas/inventory/InventoryApplication.java
+++ inventory/src/main/java/com/yas/inventory/InventoryApplication.java
@@ -1,12 +1,13 @@
 package com.yas.inventory;
 
+import com.yas.commonlibrary.config.CorsConfig;
 import com.yas.inventory.config.ServiceUrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
-@EnableConfigurationProperties(ServiceUrlConfig.class)
+@SpringBootApplication(scanBasePackages = {"com.yas.inventory", "com.yas.commonlibrary"})
+@EnableConfigurationProperties({ServiceUrlConfig.class, CorsConfig.class})
 public class InventoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
--- rating/src/main/java/com/yas/rating/RatingApplication.java
+++ rating/src/main/java/com/yas/rating/RatingApplication.java
@@ -1,12 +1,13 @@
 package com.yas.rating;
 
+import com.yas.commonlibrary.config.CorsConfig;
 import com.yas.rating.config.ServiceUrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(scanBasePackages = {"com.yas.rating", "com.yas.commonlibrary"})
-@EnableConfigurationProperties(ServiceUrlConfig.class)
+@EnableConfigurationProperties({ServiceUrlConfig.class, CorsConfig.class})
 public class RatingApplication {
 
     public static void main(String[] args) {
