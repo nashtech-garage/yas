c6941677ac8f6602b406988ae2b989e54c5b832a
Chinh Duong <chinh.duongthi@harveynash.vn>
2024-10-10 14:01:28 +0700

    #1150 [Recommendation] Adjust product detail page to show similar product
    - update UI

---
+++ storefront/common/components/SimilarProductCard.tsx
@@ -0,0 +1,52 @@
+import clsx from 'clsx';
+import Link from 'next/link';
+import { useLayoutEffect, useState } from 'react';
+
+import { getMediaById } from '@/modules/media/services/MediaService';
+import { ProductThumbnail } from 'modules/catalog/models/ProductThumbnail';
+import { formatPrice } from 'utils/formatPrice';
+import ImageWithFallBack from './ImageWithFallback';
+
+import styles from 'styles/ProductCard.module.css';
+
+export interface Props {
+  product: ProductThumbnail;
+  thumbnailUrl?: string;
+  className?: string[];
+}
+
+export default function SimilarProductCard({ product, className, thumbnailUrl }: Props) {
+  return (
+    <Link
+      className={clsx(
+        styles['product'],
+        className?.map((item) => styles[item])
+      )}
+      href={`/products/${product.slug}`}
+    >
+      <div className={styles['product-card']}>
+        <div className={styles['image-wrapper']}>
+          <ImageWithFallBack src={thumbnailUrl || ''} alt={product.name} />
+        </div>
+        <div className={styles['info-wrapper']}>
+          <h3 className={styles['prod-name']}>{product.name}</h3>
+          <div className={styles['rating-sold']}>
+            <div className={styles['star']}>
+              0 <i className="bi bi-star-fill"></i>
+            </div>{' '}
+            |{' '}
+            <div className={styles['sold']}>
+              0 <span>sold</span>
+            </div>
+          </div>
+
+          <div className={styles['price']}>{formatPrice(product.price)}</div>
+
+          <hr />
+
+          <div className={styles['delivery']}>Fast delivery 2 hours</div>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- storefront/modules/catalog/components/index.ts
+++ storefront/modules/catalog/components/index.ts
@@ -1,5 +1,6 @@
 import DetailHeader from './DetailHeader';
 import ProductDetails from './ProductDetails';
 import RelatedProduct from './RelatedProducts';
+import SimilarProduct from './SimilarProduct';
 
-export { DetailHeader, ProductDetails, RelatedProduct };
+export { DetailHeader, ProductDetails, RelatedProduct, SimilarProduct};
