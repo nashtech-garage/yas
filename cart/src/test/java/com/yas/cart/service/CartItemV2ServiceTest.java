adef73a073cead550d9b490e50f4bae0036386cc
Nguyễn Trí Hải <mochacr0@gmail.com>
2024-10-12 00:58:58 +0700

    remove unused imports

--- cart/src/test/java/com/yas/cart/service/CartItemV2ServiceTest.java
+++ cart/src/test/java/com/yas/cart/service/CartItemV2ServiceTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
