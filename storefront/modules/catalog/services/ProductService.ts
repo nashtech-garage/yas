d14ab84b13c9f99d05940b9cee7ecb777bd80dcf
tuan.nguyentronganh <nguyentronganhtuan2102@gmail.com>
2024-10-13 19:53:34 +0700

    Reformat apiClientService(Done)

--- storefront/modules/catalog/services/ProductService.ts
+++ storefront/modules/catalog/services/ProductService.ts
@@ -4,53 +4,55 @@ import { ProductAll, ProductFeature } from '../models/ProductFeature';
 import { ProductOptionValueGet } from '../models/ProductOptionValueGet';
 import { ProductVariation } from '../models/ProductVariation';
 import { ProductsGet } from '../models/ProductsGet';
+import apiClientService from '@/common/services/ApiClientService';
 
 export async function getFeaturedProducts(pageNo: number): Promise<ProductFeature> {
-  const response = await fetch(`api/product/storefront/products/featured?pageNo=${pageNo}`);
+  const response = await apiClientService.get(
+    `api/product/storefront/products/featured?pageNo=${pageNo}`
+  );
   return response.json();
 }
 
 export async function getProductDetail(slug: string): Promise<ProductDetail> {
-  const response = await fetch(process.env.API_BASE_PATH + '/product/storefront/product/' + slug);
+  const response = await apiClientService.get(
+    process.env.API_BASE_PATH + '/product/storefront/product/' + slug
+  );
   return response.json();
 }
 
 export async function getProductOptionValues(productId: number): Promise<ProductOptionValueGet[]> {
-  const res = await fetch(
+  const res = await apiClientService.get(
     `${process.env.API_BASE_PATH}/product/storefront/product-option-combinations/${productId}/values`
   );
   if (res.status >= 200 && res.status < 300) return res.json();
-  return Promise.reject(res);
+  throw res;
 }
 
 export async function getProductByMultiParams(queryString: string): Promise<ProductAll> {
-  const res = await fetch(`/api/product/storefront/products?${queryString}`);
+  const res = await apiClientService.get(`/api/product/storefront/products?${queryString}`);
   return res.json();
 }
 
 export async function getProductVariationsByParentId(
   parentId: number
 ): Promise<ProductVariation[]> {
-  const res = await fetch(
+  const res = await apiClientService.get(
     `${process.env.API_BASE_PATH}/product/storefront/product-variations/${parentId}`
   );
   if (res.status >= 200 && res.status < 300) return res.json();
-  return Promise.reject(res);
+  throw res;
 }
 
 export async function getProductSlug(productId: number): Promise<ProductSlug> {
-  const res = await fetch(`/api/product/storefront/productions/${productId}/slug`);
+  const res = await apiClientService.get(`/api/product/storefront/productions/${productId}/slug`);
   if (res.status >= 200 && res.status < 300) return res.json();
-  return Promise.reject(res);
+  throw res;
 }
 
 export async function getRelatedProductsByProductId(productId: number): Promise<ProductsGet> {
-  const res = await fetch(`/api/product/storefront/products/related-products/${productId}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const res = await apiClientService.get(
+    `/api/product/storefront/products/related-products/${productId}`
+  );
   if (res.status >= 200 && res.status < 300) return res.json();
-  return Promise.reject(res);
+  throw res;
 }
