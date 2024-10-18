cd9ef709eedfec60fc94304e97d38a696bc24690
Duy Le Van <duy.levan@nashtechglobal.com>
2024-10-18 15:41:39 +0700

    #1199 - add ProductVectorRepositoryTest

--- recommendation/src/it/java/com/yas/recommendation/TestConfig.java
+++ recommendation/src/it/java/com/yas/recommendation/TestConfig.java
@@ -2,8 +2,10 @@ package com.yas.recommendation;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 @Import(KafkaIntegrationTestConfiguration.class)
+@TestPropertySource("classpath:application-test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestConfig {
 }
