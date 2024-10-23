de556fdf3a3971e79fa937e1e131e1ea353dc9fc
DinhThaiVV <112363597+DinhThaiVV@users.noreply.github.com>
2024-10-23 13:54:06 +0700

    Feature/1175 delivery service (#1200)
    * feature/1175-delivery-service
    
    * feature/1175-delivery-service

---
+++ delivery/src/main/java/com/yas/delivery/DeliveryApplication.java
@@ -0,0 +1,11 @@
+package com.yas.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryApplication.class, args);
+    }
+}
