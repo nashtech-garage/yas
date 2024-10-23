b87f06f624c53d69012835d138618485fa792e19
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2024-10-23 11:36:33 +0700

    #1196 fix issue missing payment method and additional validation other fields

--- order/src/main/java/com/yas/order/viewmodel/order/OrderPostVm.java
+++ order/src/main/java/com/yas/order/viewmodel/order/OrderPostVm.java
@@ -4,6 +4,7 @@ import com.yas.order.model.enumeration.DeliveryMethod;
 import com.yas.order.model.enumeration.PaymentMethod;
 import com.yas.order.model.enumeration.PaymentStatus;
 import com.yas.order.viewmodel.orderaddress.OrderAddressPostVm;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
@@ -11,20 +12,20 @@ import lombok.Builder;
 
 @Builder
 public record OrderPostVm(
-        String checkoutId,
-        String email,
-        OrderAddressPostVm shippingAddressPostVm,
-        OrderAddressPostVm billingAddressPostVm,
+        @NotBlank String checkoutId,
+        @NotBlank String email,
+        @NotNull OrderAddressPostVm shippingAddressPostVm,
+        @NotNull OrderAddressPostVm billingAddressPostVm,
         String note,
         float tax,
         float discount,
         int numberItem,
-        BigDecimal totalPrice,
+        @NotNull BigDecimal totalPrice,
         BigDecimal deliveryFee,
         String couponCode,
-        DeliveryMethod deliveryMethod,
-        PaymentMethod paymentMethod,
-        PaymentStatus paymentStatus,
+        @NotNull DeliveryMethod deliveryMethod,
+        @NotNull PaymentMethod paymentMethod,
+        @NotNull PaymentStatus paymentStatus,
         @NotNull
         List<OrderItemPostVm> orderItemPostVms
 ) {
