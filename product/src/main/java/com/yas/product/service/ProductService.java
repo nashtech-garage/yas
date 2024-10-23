f4da0431fd5d41053f136993a7fe2ede23f155c4
Du Luong Khanh <du.luongkhanh@nashtechglobal.com>
2024-10-23 14:26:49 +0700

    change to builder

--- product/src/main/java/com/yas/product/service/ProductService.java
+++ product/src/main/java/com/yas/product/service/ProductService.java
@@ -649,35 +649,36 @@ public class ProductService {
         if (null != product.getBrand()) {
             brandId = product.getBrand().getId();
         }
-        return new ProductDetailVm(product.getId(),
-            product.getName(),
-            product.getShortDescription(),
-            product.getDescription(),
-            product.getSpecification(),
-            product.getSku(),
-            product.getGtin(),
-            product.getSlug(),
-            product.isAllowedToOrder(),
-            product.isPublished(),
-            product.isFeatured(),
-            product.isVisibleIndividually(),
-            product.isStockTrackingEnabled(),
-            product.getWeight(),
-            product.getDimensionUnit(),
-            product.getLength(),
-            product.getWidth(),
-            product.getHeight(),
-            product.getPrice(),
-            brandId,
-            categories,
-            product.getMetaTitle(),
-            product.getMetaKeyword(),
-            product.getMetaDescription(),
-            thumbnailMedia,
-            productImageMedias,
-            product.getTaxClassId(),
-            Objects.isNull(product.getParent()) ? null : product.getParent().getId()
-        );
+        return ProductDetailVm.builder()
+            .id(product.getId())
+            .name(product.getName())
+            .shortDescription(product.getShortDescription())
+            .description(product.getDescription())
+            .specification(product.getSpecification())
+            .sku(product.getSku())
+            .gtin(product.getGtin())
+            .slug(product.getSlug())
+            .isAllowedToOrder(product.isAllowedToOrder())
+            .isPublished(product.isPublished())
+            .isFeatured(product.isFeatured())
+            .isVisible(product.isVisibleIndividually())
+            .stockTrackingEnabled(product.isStockTrackingEnabled())
+            .weight(product.getWeight())
+            .dimensionUnit(product.getDimensionUnit())
+            .length(product.getLength())
+            .width(product.getWidth())
+            .height(product.getHeight())
+            .price(product.getPrice())
+            .brandId(brandId)
+            .categories(categories)
+            .metaTitle(product.getMetaTitle())
+            .metaKeyword(product.getMetaKeyword())
+            .metaDescription(product.getMetaDescription())
+            .thumbnailMedia(thumbnailMedia)
+            .productImageMedias(productImageMedias)
+            .taxClassId(product.getTaxClassId())
+            .parentId(Objects.isNull(product.getParent()) ? null : product.getParent().getId())
+            .build();
     }
 
     public List<ProductListVm> getLatestProducts(int count) {
