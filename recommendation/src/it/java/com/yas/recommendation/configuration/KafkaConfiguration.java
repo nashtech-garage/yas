c269e4602a352311aefb491005e909082de6ee49
Chinh Duong <chinh.duongthi@harveynash.vn>
2024-10-24 19:06:29 +0700

    #1199 - IT for recommendation service
    - add integration test

---
+++ recommendation/src/it/java/com/yas/recommendation/configuration/KafkaConfiguration.java
@@ -0,0 +1,33 @@
+package com.yas.recommendation.configuration;
+
+import common.container.ContainerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration
+public class KafkaConfiguration {
+    @Value("${kafka.version}")
+    private String kafkaVersion;
+
+    @Value("${pgvector.version}")
+    private String pgVectorVersion;
+
+    @Bean
+    @ServiceConnection
+    public KafkaContainer kafkaContainer(DynamicPropertyRegistry registry) {
+        return ContainerFactory.kafkaContainer(registry, kafkaVersion);
+    }
+
+    @Bean
+    @ServiceConnection
+    public PostgreSQLContainer pgvectorContainer(DynamicPropertyRegistry registry) {
+        return ContainerFactory.pgvector(registry, pgVectorVersion);
+    }
+
+
+}
