2dd9767fbcc8818df45007468b42818a25b92316
huy.phamphuvu <huy.phamphuvu@nashtechglobal.com>
2024-10-29 11:21:37 +0700

    Update payment methods to Checkout Object

--- order/src/main/java/com/yas/order/controller/CheckoutController.java
+++ order/src/main/java/com/yas/order/controller/CheckoutController.java
@@ -42,7 +42,7 @@ public class CheckoutController {
         return ResponseEntity.ok(checkoutService.getCheckoutPendingStateWithItemsById(id));
     }
 
-    @PutMapping("/storefront/checkouts/{id}/payment")
+    @PutMapping("/storefront/checkouts/{id}/payment-method")
     @ApiResponses(value = {
         @ApiResponse(responseCode = ApiConstant.CODE_200, description = ApiConstant.OK,
             content = @Content()),
