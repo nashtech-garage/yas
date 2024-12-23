c054317041b047166fc086bc7d2cd119de06bdf8
Nguyễn Trí Hải <mochacr0@gmail.com>
2024-12-24 01:21:35 +0700

    disable JPA auto configuration

--- delivery/src/main/java/com/yas/delivery/DeliveryApplication.java
+++ delivery/src/main/java/com/yas/delivery/DeliveryApplication.java
@@ -1,9 +1,17 @@
 package com.yas.delivery;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = {"com.yas.delivery", "com.yas.commonlibrary"})
+@EnableAutoConfiguration(exclude = {
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class})
 public class DeliveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeliveryApplication.class, args);
