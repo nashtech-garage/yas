d47438176fa13faaa89dded81465af3751200f18
Hoang Dang <hoangdang.sn0704@gmail.com>
2026-03-15 18:14:48 +0700

    rename intergration tests in recommendation module

--- recommendation/src/it/java/com/yas/recommendation/controller/EmbeddingQueryControllerIT.java
+++ recommendation/src/it/java/com/yas/recommendation/controller/EmbeddingQueryControllerIT.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(EmbeddingQueryController.class)
 @AutoConfigureMockMvc(addFilters = false)
-class EmbeddingQueryControllerTest {
+class EmbeddingQueryControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
