9b758522708ec622b072fb3d1b9fc6b59ded684a
Hoang Dang <hoangdang.sn0704@gmail.com>
2026-03-16 23:44:19 +0700

    remove secrets from code (values.yaml) and replace with env vars, also update the deploy script to use env vars instead of hardcoded secrets

--- k8s/deploy/deploy-yas-configuration.sh
+++ k8s/deploy/deploy-yas-configuration.sh
@@ -6,6 +6,28 @@ helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo update
 
 helm dependency build ../charts/yas-configuration
-helm upgrade --install yas-configuration ../charts/yas-configuration \
---namespace yas --create-namespace
+
+HELM_SECRET_ARGS=""
+
+if [ -n "${KEYCLOAK_BACKOFFICE_BFF_CLIENT_SECRET:-}" ]; then
+	HELM_SECRET_ARGS="$HELM_SECRET_ARGS --set-string credentials.keycloak.backofficeBffClientSecret=${KEYCLOAK_BACKOFFICE_BFF_CLIENT_SECRET}"
+fi
+
+if [ -n "${KEYCLOAK_STOREFRONT_BFF_CLIENT_SECRET:-}" ]; then
+	HELM_SECRET_ARGS="$HELM_SECRET_ARGS --set-string credentials.keycloak.storefrontBffClientSecret=${KEYCLOAK_STOREFRONT_BFF_CLIENT_SECRET}"
+fi
+
+if [ -n "${KEYCLOAK_CUSTOMER_MANAGEMENT_CLIENT_SECRET:-}" ]; then
+	HELM_SECRET_ARGS="$HELM_SECRET_ARGS --set-string credentials.keycloak.customerManagementClientSecret=${KEYCLOAK_CUSTOMER_MANAGEMENT_CLIENT_SECRET}"
+fi
+
+if [ -n "$HELM_SECRET_ARGS" ]; then
+	# shellcheck disable=SC2086
+	helm upgrade --install yas-configuration ../charts/yas-configuration \
+	--namespace yas --create-namespace \
+	$HELM_SECRET_ARGS
+else
+	helm upgrade --install yas-configuration ../charts/yas-configuration \
+	--namespace yas --create-namespace
+fi
 
