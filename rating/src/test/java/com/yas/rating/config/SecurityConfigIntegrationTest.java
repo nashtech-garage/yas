14013b3d8236c9a4d3ac1a0aa4d2987cba340491
TienThanhNguyen <nguyentienthanhaeaefff@gmail.com>
2026-04-30 07:12:32 +0700

    Fix test context bean

--- rating/src/test/java/com/yas/rating/config/SecurityConfigIntegrationTest.java
+++ rating/src/test/java/com/yas/rating/config/SecurityConfigIntegrationTest.java
@@ -9,7 +9,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.yas.rating.controller.RatingController;
 import com.yas.rating.service.RatingService;
 import com.yas.rating.viewmodel.RatingListVm;
 import java.time.Instant;
@@ -18,15 +17,15 @@ import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = RatingController.class)
-@Import(SecurityConfig.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class SecurityConfigIntegrationTest {
 
     @MockitoBean
