717f0058df06fedf7fbd2f1c1335c73bb561b8d4
minhkhang1505 <mkhang040506@gmail.com>
2026-06-23 10:39:42 +0700

    build: disable eslint/typescript checks during docker build for storefront to prevent OOM

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
       },
     ],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
