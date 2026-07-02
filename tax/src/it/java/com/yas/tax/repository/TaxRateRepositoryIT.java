c0aa5c0caf80c536591e064e87e2b05c655aff0c
NhatDat <nhatdat2905@gmail.com>
2026-07-02 18:07:52 +0700

    án

--- tax/src/it/java/com/yas/tax/repository/TaxRateRepositoryIT.java
+++ tax/src/it/java/com/yas/tax/repository/TaxRateRepositoryIT.java
@@ -3,7 +3,7 @@ package com.yas.tax.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.instancio.Select.field;
 //test
-//
+//abc
 import com.yas.commonlibrary.IntegrationTestConfiguration;
 import com.yas.tax.model.TaxClass;
 import com.yas.tax.model.TaxRate;
