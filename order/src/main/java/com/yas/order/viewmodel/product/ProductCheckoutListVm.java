1b915ee17bdc439aced10e6f23cafb515f1d6269
Manh Trong Kien <manhtrongkien1901@gmail.com>
2026-07-06 20:05:49 +0700

    update ProductCheckoutListVM

--- order/src/main/java/com/yas/order/viewmodel/product/ProductCheckoutListVm.java
+++ order/src/main/java/com/yas/order/viewmodel/product/ProductCheckoutListVm.java
@@ -3,11 +3,23 @@ package com.yas.order.viewmodel.product;
 import lombok.Builder;
 import lombok.Data;
 
-@Builder(toBuilder = true)
+// @Builder(toBuilder = true)
+// @Data
+// public class ProductCheckoutListVm {
+//     Long id;
+//     String name;
+//     Double price;
+//     Long taxClassId;
+// }
+
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductCheckoutListVm {
-    Long id;
-    String name;
-    Double price;
-    Long taxClassId;
+
+    private Long id;
+    private String name;
+    private Double price;
+    private Long taxClassId;
 }
