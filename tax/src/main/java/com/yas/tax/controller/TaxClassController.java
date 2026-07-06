fe83a6be03cbf8a2f9bc3c7d3d0b6af52d16f35a
23120292 <nakrothnguyen127@gmail.com>
2026-07-06 18:08:26 +0700

    m

--- tax/src/main/java/com/yas/tax/controller/TaxClassController.java
+++ tax/src/main/java/com/yas/tax/controller/TaxClassController.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 // test cicd
+// oke không
 @RestController
 @RequestMapping(ApiConstant.TAX_CLASS_URL)
 public class TaxClassController {
